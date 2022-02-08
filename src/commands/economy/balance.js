23bc12fc344346c16e255428d49c482b14841a2a
Siebe Baree <siebe.baree@outlook.com>
2022-02-08 22:42:01 +0100

    Initial commit

---
+++ src/commands/economy/balance.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.execute = async (client, interaction, data) => {
+    const member = interaction.options.getUser('user') || interaction.member;
+    if (member.bot) return interaction.reply({ content: 'That user is a bot. You can only check the balance of a real person.', ephemeral: true });
+    const memberData = await client.database.fetchGuildUser(interaction.guildId, member.id);
+
+    const embed = new MessageEmbed()
+        .setAuthor({ name: `${member.displayName || member.username}'s balance`, iconURL: `${member.displayAvatarURL() || client.config.embed.defaultIcon}` })
+        .setColor(client.config.embed.color)
+        .setFooter({ text: client.config.embed.footer })
+        .addFields(
+            { name: 'Wallet', value: `${memberData.wallet}`, inline: true },
+            { name: 'Bank', value: `${memberData.bank}`, inline: true },
+            { name: 'Net Worth', value: `${memberData.wallet + memberData.bank}`, inline: true }
+        )
+    await interaction.reply({ embeds: [embed] });
+}
+
+module.exports.help = {
+    name: "balance",
+    description: "Get your balance or the balance of another user.",
+    options: [
+        {
+            name: 'user',
+            type: 'USER',
+            description: 'Get the balance of another user.',
+            required: false
+        }
+    ],
+    usage: "",
+    category: "economy",
+    extraFields: [],
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY"],
+    ownerOnly: false,
+    cooldown: 3,
+    enabled: true
+}
