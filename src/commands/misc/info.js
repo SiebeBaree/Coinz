c104c0670a62edc3893b75eb7c6d39150da093d1
Siebe Baree <siebe.baree@outlook.com>
2022-02-08 22:21:07 +0100

    Added shard info

--- src/commands/misc/info.js
+++ src/commands/misc/info.js
@@ -12,7 +12,7 @@ module.exports.execute = async (client, interaction, data) => {
         .setDescription('**If you like this bot, maybe consider [donating](https://coinzbot.xyz/donate).**')
         .addFields(
             { name: 'Info', value: `:man_technologist: **Developer:** \`Siebe#9999\`\n:globe_with_meridians: **Website: [coinzbot.xyz](${client.config.website})**\n:beginner: **Official Server: [discord.gg/asnZQwc6kW](https://discord.gg/asnZQwc6kW)**\n:books: **Library:** \`discord.js${dependencies["discord.js"]}\``, inline: true },
-            { name: 'Statistics', value: `:video_game: **Commands:** \`${client.commands.size}\`\n:white_check_mark: **Uptime:** \`${client.calc.msToTime(client.uptime)}\`\n:film_frames: **Memory Usage:** \`${Math.round(usedMemory * 100) / 100}\` MB`, inline: true }
+            { name: 'Statistics', value: `:video_game: **Commands:** \`${client.commands.size}\`\n:spider_web: **Shard:** \`${interaction.guild.shardId + 1}/${client.shard.count}\`\n:white_check_mark: **Uptime:** \`${client.calc.msToTime(client.uptime)}\`\n:film_frames: **Memory Usage:** \`${Math.round(usedMemory * 100) / 100}\` MB`, inline: true }
         )
     await interaction.reply({ embeds: [newEmbed] });
 }
