2f2c1f2c354b440e33795b76ffd832c20f0bbb1d
Siebe Baree <siebe.baree@outlook.com>
2022-02-09 11:14:20 +0100

    Added new fields

--- src/database/schemas/shop.js
+++ src/database/schemas/shop.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 
 module.exports = mongoose.model("Shop", new mongoose.Schema({
     itemId: { type: String, require: true },
+    category: { type: String, require: true },
     name: { type: String, require: true },
     icon: { type: String, require: true },
-    description: { type: String, require: true },
+    shortDescription: { type: String, require: true },
+    longDescription: String,
     buyPrice: { type: Number, require: true },
     sellPrice: { type: Number, require: true },
     type: { type: String, default: "None" },
