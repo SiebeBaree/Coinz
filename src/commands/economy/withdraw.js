79ca8f63d2a59663f75de1743899abbf3c3426f4
Siebe Baree <siebe.baree@outlook.com>
2022-02-09 10:36:33 +0100

    Initial commit

---
+++ src/commands/economy/withdraw.js
@@ -0,0 +1,60 @@
+const { MessageEmbed } = require('discord.js');
+const guildUserSchema = require('../../database/schemas/guildUsers');
+
+module.exports.execute = async (client, interaction, data) => {
+    let amount = interaction.options.getString('amount') || "0";
+
+    if (amount.toLowerCase() === "all") {
+        amount = data.guildUser.bank;
+    } else {
+        try {
+            amount = parseInt(amount);
+
+            if (amount > data.guildUser.bank) {
+                return interaction.reply({ content: `You don't have that much in your bank. You only have :coin: ${data.guildUser.bank}.`, ephemeral: true });
+            } else if (amount <= 0) {
+                return interaction.reply({ content: `You need to withdraw at least :coin: 1.`, ephemeral: true });
+            }
+        } catch (e) {
+            return interaction.reply({ content: `Invalid amount. Please enter a number or type \`ALL\``, ephemeral: true });
+        }
+    }
+
+    await guildUserSchema.updateOne({ guildId: interaction.guildId, userId: interaction.member.id }, {
+        $inc: {
+            wallet: amount,
+            bank: -amount
+        }
+    });
+
+    const embed = new MessageEmbed()
+        .setAuthor({ name: `Withdrawn Money`, iconURL: `${interaction.member.displayAvatarURL() || client.config.embed.defaultIcon}` })
+        .setColor(client.config.embed.color)
+        .setDescription(`Successfully withdrawn :coin: ${amount} from your bank.`)
+        .addFields(
+            { name: 'New Wallet Balance', value: `:coin: ${data.guildUser.wallet + amount}`, inline: true },
+            { name: 'New Bank Balance', value: `:coin: ${data.guildUser.bank - amount}`, inline: true }
+        )
+    await interaction.reply({ embeds: [embed] });
+}
+
+module.exports.help = {
+    name: "withdraw",
+    description: "Withdraw money from your bank to your wallet.",
+    options: [
+        {
+            name: 'amount',
+            type: 'STRING',
+            description: 'Enter an amount that you want to withdraw or ALL to withdraw all available money in your bank.',
+            required: true
+        }
+    ],
+    usage: "",
+    category: "economy",
+    extraFields: [],
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY"],
+    ownerOnly: false,
+    cooldown: 3,
+    enabled: true
+}
