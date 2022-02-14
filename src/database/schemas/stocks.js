0b8c75225a3cbd55b97434af190f6b1a9eb76188
Siebe Baree <siebe.baree@outlook.com>
2022-02-14 21:23:50 +0100

    Cleaned up stocks.js

--- src/database/schemas/stocks.js
+++ src/database/schemas/stocks.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
-const moment = require('moment')
 
 module.exports = mongoose.model("Stocks", new mongoose.Schema({
     ticker: { type: String, require: true },
     type: { type: String, require: true },
     fullName: { type: String, require: true },
-    description: { type: String, require: true },
-    price: { type: Number, require: true },
-    previous_price: { type: Number, default: 0 },
-    lastUpdated: { type: Date, default: Date.now }
+    price: { type: mongoose.Types.Decimal128, require: true },
+    previousClose: { type: mongoose.Types.Decimal128, default: 0 },
+    lastUpdated: { type: Number, default: 0 }
 }))
