c02bb60102c4bb829d9e9493deb69c68541ab163
Siebe Baree <siebe.baree@outlook.com>
2022-02-14 21:24:34 +0100

    Added API to get stock information

--- src/tools/tools.js
+++ src/tools/tools.js
@@ -1,6 +1,16 @@
 const guildUserSchema = require('../database/schemas/guildUsers');
 const database = require('../database/mongoose');
 const { MessageActionRow, MessageButton } = require('discord.js');
+const axios = require("axios").default;
+require('dotenv').config();
+
+// Because our Stocks API has a rate limit of 1000 requests per month we need to bundle the stocks per groups of 10.
+// Every group only counts as 1 request. Thats 20 stocks updated every 1.5 hours.
+// Later for $10/month (15000 requests per month) we can upgrade the API to allow 50 stocks updated every 15 minutes
+const stocks = [
+    ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'TSLA', 'NVDA', 'FB', 'PFE', 'DIS', 'KO'],
+    ['ADBE', 'INTC', 'MCD', 'NKE', 'DHR', 'NFLX', 'BA', 'BLK', 'GM', 'FDX']
+];
 
 module.exports.addMoney = async (guildId, userId, amount) => {
     await guildUserSchema.updateOne({ guildId: guildId, userId: userId }, {
@@ -53,3 +63,33 @@ module.exports.setListButtons = (currentPage, maxPages, disableAll = false) => {
     );
     return row;
 };
+
+module.exports.getNewStockData = async (ticker) => {
+    let symbols = "";
+    for (const stockList in stocks) {
+        if (stocks[stockList].includes(ticker)) {
+            symbols = stocks[stockList].join(',');
+            break;
+        }
+    }
+
+    if (symbols !== "") {
+        let options = {
+            method: 'GET',
+            url: 'https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v8/finance/spark',
+            params: { symbols: symbols, range: '1d', interval: '15m' },
+            headers: {
+                'x-rapidapi-host': 'stock-data-yahoo-finance-alternative.p.rapidapi.com',
+                'x-rapidapi-key': `${process.env.STOCK_API_KEY}`
+            }
+        };
+
+        try {
+            const response = await axios.request(options);
+            return response.data;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    return null;
+}
