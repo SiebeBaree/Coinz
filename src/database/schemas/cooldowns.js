40ad8c39b89c5d69c1512ad6eba70092fec328f2
Siebe Baree <siebe.baree@outlook.com>
2022-02-18 18:39:20 +0100

    Removed timesUsed

--- src/database/schemas/cooldowns.js
+++ src/database/schemas/cooldowns.js
@@ -4,6 +4,5 @@ module.exports = mongoose.model("Cooldowns", new mongoose.Schema({
     guildId: { type: String, require: true },
     userId: { type: String, require: true },
     command: { type: String, require: true },
-    expiresOn: { type: Number, default: 0 }, // Epoch Unix Timestamp (Format: Seconds)
-    timesUsed: { type: Number, default: 1 }
+    expiresOn: { type: Number, default: 0 } // Epoch Unix Timestamp (Format: Seconds)
 }))
