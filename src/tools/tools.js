638b8bbff2852b2c8e6c8f2afc53197e12ae52d7
Siebe Baree <siebe.baree@outlook.com>
2022-02-20 16:42:17 +0100

    removed stocks tools

--- src/tools/tools.js
+++ src/tools/tools.js
@@ -1,14 +1,6 @@
 const guildUserSchema = require('../database/schemas/guildUsers');
 const database = require('../database/mongoose');
 const { MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
-const axios = require("axios").default;
-require('dotenv').config();
-const fs = require('fs');
-
-// Because our Stocks API has a rate limit of 1000 requests per month we need to bundle the stocks per groups of 10.
-// Every group only counts as 1 request. Thats 20 stocks updated every 75 minutes ~ 883 requests/month (+ 8 weekend days)
-// Later for $10/month (15000 requests per month) we can upgrade the API to allow 60 stocks updated every 15 minutes ~ 13.248 requests/month (+ 8 weekend days)
-const stocks = require('../data/market/stocks.json').stocks;
 
 module.exports.addMoney = async (guildId, userId, amount) => {
     await guildUserSchema.updateOne({ guildId: guildId, userId: userId }, {
@@ -90,53 +82,6 @@ module.exports.createSelectMenu = (defaultLabel, disabled = false) => {
                 .addOptions(options),
         );
     return selectMenu;
-}
-
-module.exports.getNewStockData = async (ticker) => {
-    let symbols = "";
-    for (const stockList in stocks) {
-        if (stocks[stockList].includes(ticker)) {
-            symbols = stocks[stockList].join(',');
-            break;
-        }
-    }
-
-    if (symbols !== "") {
-        let options = {
-            method: 'GET',
-            url: 'https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v8/finance/spark',
-            params: { symbols: symbols, range: '1d', interval: '15m' },
-            headers: {
-                'x-rapidapi-host': 'stock-data-yahoo-finance-alternative.p.rapidapi.com',
-                'x-rapidapi-key': `${process.env.STOCK_API_KEY}`
-            }
-        };
-
-        try {
-            const response = await axios.request(options);
-            const apiData = response.data;
-
-            const markets = require('../data/market/markets.json');
-
-            markets.marketStart = apiData.start;
-            markets.marketClose = apiData.end;
-
-            fs.writeFile(`${process.cwd()}/src/data/market/markets.json`, JSON.stringify(markets, null, 4), function writeJSON(err) {
-                if (err) return console.log(err);
-            });
-
-            return apiData;
-        } catch (e) {
-            console.log(e);
-        }
-    }
-    return null;
-}
-
-module.exports.marketIsOpen = async () => {
-    const now = parseInt(Date.now() / 1000);
-    const marketInfo = require('../data/market/markets.json');
-    return (now >= marketInfo.marketStart && now <= marketInfo.marketClose ? true : false)
 }
 
 module.exports.userHasItem = async (inv, itemId) => {
