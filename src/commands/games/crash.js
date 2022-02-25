4d56ed89e71787c37d5de2305cc433c78d992438
Siebe Baree <siebe.baree@outlook.com>
2022-02-25 15:08:03 +0100

    Small bug fix

--- src/commands/games/crash.js
+++ src/commands/games/crash.js
@@ -27,7 +27,7 @@ function setButton(buttonIsDisabled = false) {
 module.exports.execute = async (client, interaction, data) => {
     const bet = interaction.options.getInteger('bet');
     if (bet > data.guildUser.wallet) {
-        await client.cooldown.removeCooldown(interaction.guildId, interaction.member.id, "crash");
+        await client.cooldown.removeCooldown(interaction.guildId, interaction.member.id, data.help.name);
         return interaction.reply({ content: `You don't have :coin: ${bet} in your wallet.`, ephemeral: true });
     }
 
