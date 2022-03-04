41fe4f3abcf417915da2f1c88f6a3a1597f1f0e5
Siebe Baree <siebe.baree@outlook.com>
2022-03-04 14:24:56 +0100

    Fixed msToTime calculator

--- src/tools/calculators.js
+++ src/tools/calculators.js
@@ -1,17 +1,17 @@
-module.exports.msToTime = ms => {
-    const days = Math.floor(ms / (1000 * 60 * 60 * 60));
-    const hours = Math.floor(ms / (1000 * 60 * 60) % 60);
-    const minutes = Math.floor(ms / (1000 * 60) % 60);
-    const seconds = Math.floor(ms / (1000) % 60);
+module.exports.msToTime = duration => {
+    let seconds = Math.floor((duration / 1000) % 60);
+    let minutes = Math.floor((duration / (1000 * 60)) % 60);
+    let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    let days = Math.floor((duration / (1000 * 60 * 60 * 24)));
 
     let str = "";
-    if (days) str = str + days + "d ";
-    if (hours) str = str + hours + "h ";
-    if (minutes) str = str + minutes + "m ";
-    if (seconds) str = str + seconds + "s";
+    str += days > 0 ? days + "d " : "";
+    str += hours > 0 ? hours + "h " : "";
+    str += minutes > 0 ? minutes + "m " : "";
+    str += seconds > 0 ? seconds + "s" : "";
 
     return str || "0s";
-};
+}
 
 // Not 100% accurate but it doesn't need to be very accurate.
 module.exports.roundNumber = (n, places = 2) => {
