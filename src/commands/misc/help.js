e2431e8a88b61a491d555253d9f94fa15963870b
Siebe Baree <siebe.baree@outlook.com>
2022-03-04 15:36:01 +0100

    Added new fields to embed

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -4,12 +4,12 @@ const categories = [
     { name: "Miscellaneous", category: "misc", icon: ":wrench: " },
     { name: "Economy", category: "economy", icon: ":coin: " },
     { name: "Games", category: "games", icon: ":video_game: " },
-    { name: "Pets", category: "pets", icon: ":dog: " },
-    { name: "Business", category: "business", icon: ":office: " },
+    // { name: "Pets", category: "pets", icon: ":dog: " },
+    // { name: "Business", category: "business", icon: ":office: " },
     { name: "Crop Farming", category: "farming", icon: ":carrot: " },
     { name: "Stocks", category: "stocks", icon: ":chart_with_upwards_trend: " },
-    { name: "Social Media", category: "social", icon: ":mobile_phone: " },
-    { name: "Crypto Mining", category: "mining", icon: ":zap: " }
+    // { name: "Social Media", category: "social", icon: ":mobile_phone: " },
+    // { name: "Crypto Mining", category: "mining", icon: ":zap: " }
 ];
 
 module.exports.execute = async (client, interaction, data) => {
@@ -41,29 +41,49 @@ module.exports.execute = async (client, interaction, data) => {
             });
         } else {
             const command = client.commands.get(commandName.toLowerCase());
-
             if (!command) return interaction.reply({ content: `Sorry, we couldn't find any category or command with the name \`${commandName}\`.`, ephemeral: true })
+
+            const options = command.help.options;
+            let usage = "";
+            for (let i = 0; i < options.length; i++) {
+                if (options[i].type === "SUB_COMMAND") {
+                    usage += `/${command.help.name} ${options[i].name}`;
+                    for (let j = 0; j < options[i].options.length; j++) {
+                        usage += `${options[i].options[j].required ? " <" : " ["}${options[i].options[j].name}${options[i].options[j].required ? ">" : "]"}`
+                    }
+                    usage += "\n";
+                } else {
+                    if (i === 0) usage += `/${command.help.name}`;
+                    usage += `${options[i].required ? " <" : " ["}${options[i].name}${options[i].required ? ">" : "]"}`
+                }
+            }
 
             var embed = new MessageEmbed()
                 .setAuthor({ name: `Help: ${command.help.name}`, iconURL: `${client.user.avatarURL() || client.config.embed.defaultIcon}` })
                 .setColor(client.config.embed.color)
                 .setFooter({ text: client.config.embed.footer })
                 .setDescription(command.help.description || "No Description.")
-
-            if (command.help.usage == "") {
-                embed.addField('Command Usage', `\`/${command.help.name}\``, false)
-            } else {
-                embed.addField('Command Usage', `\`/${command.help.name} ${command.help.usage}\``, false)
-            }
-
-            embed.addField('Cooldown', client.calc.msToTime(command.help.cooldown * 1000), false);
+                .addFields(
+                    { name: 'Command Usage', value: `\`${usage === "" ? "/" + command.help.name : usage}\``, inline: false },
+                    { name: 'Cooldown', value: client.calc.msToTime(command.help.cooldown * 1000), inline: false }
+                )
 
             let perms = [];
             command.help.botPermissions.forEach(perm => {
                 perms.push(`\`${perm}\``);
             });
 
-            embed.addField('Permissions Needed', perms.join(', '), false)
+            embed.addField('Permissions Needed', perms.join(', '), false);
+
+            if (command.help.extraFields !== undefined && command.help.extraFields.length > 0) {
+                command.help.extraFields.forEach(field => {
+                    embed.addField(field.name, field.value, field.inline);
+                });
+            }
+
+            if (command.help.image !== undefined && command.help.image != "") {
+                embed.setImage(command.help.image);
+            }
         }
 
         await interaction.reply({ embeds: [embed] });
@@ -93,7 +113,6 @@ module.exports.help = {
             required: false
         }
     ],
-    usage: "[category | command]",
     category: "misc",
     extraFields: [],
     memberPermissions: [],
