99e56e0cac5d25ae4fb4350b008451dcb0d0a82f
Siebe Baree <siebe.baree@outlook.com>
2022-03-11 11:10:08 +0100

    Initial commit

---
+++ website/webApp/dashboard/routes.py
@@ -0,0 +1,45 @@
+from webApp.utils import is_authorized, is_guild_valid
+from flask import render_template, session, redirect, url_for, Blueprint, abort
+
+dashboard = Blueprint('dashboard', __name__)
+
+
+@dashboard.route("/")
+def dash():
+    if not is_authorized():
+        return redirect(url_for("login.login_page"))
+
+    user = session.get("user")
+    valid_guilds = session.get("valid")
+
+    guilds = []
+    index = -1
+    for i in range(0, len(valid_guilds)):
+        if i % 3 == 0:
+            index += 1
+            guilds.append([])
+
+        guilds[index].append(valid_guilds[i])
+
+    return render_template('dashboard.html', title="Dashboard", logged_in=True, user=user, guilds=guilds)
+
+
+@dashboard.route("/<guild_id>")
+def dash_guild(guild_id):
+    if not is_authorized():
+        return redirect(url_for("login.login_page"))
+    if not is_guild_valid(guild_id, session.get("valid")):
+        return abort(403)
+
+    user = session.get("user")
+
+    guild = None
+    for guild_obj in session.get('valid'):
+        if guild_obj['id'] == guild_id:
+            guild = guild_obj
+            break
+
+    if guild is None:
+        return abort(404)
+
+    return render_template('dashboard_guild.html', title="Dashboard", logged_in=True, user=user, guild=guild)
