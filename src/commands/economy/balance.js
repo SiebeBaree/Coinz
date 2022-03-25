546b437a9fca24f9865a825379e521564a0feefc
Siebe Baree <siebe.baree@outlook.com>
2022-03-25 16:53:30 +0100

    Added inventory worth

--- src/commands/economy/balance.js
+++ src/commands/economy/balance.js
@@ -3,7 +3,16 @@ const { MessageEmbed } = require('discord.js');
 module.exports.execute = async (client, interaction, data) => {
     const member = interaction.options.getUser('user') || interaction.member;
     if (member.bot) return interaction.reply({ content: 'That user is a bot. You can only check the balance of a real person.', ephemeral: true });
+    await interaction.deferReply();
     const memberData = await client.database.fetchGuildUser(interaction.guildId, member.id);
+
+    let invValue = 0;
+    if (memberData.inventory !== undefined) {
+        for (let i = 0; i < memberData.inventory.length; i++) {
+            const item = await client.database.fetchItem(memberData.inventory[i].itemId);
+            invValue += parseInt(item.sellPrice * memberData.inventory[i].quantity);
+        }
+    }
 
     const embed = new MessageEmbed()
         .setAuthor({ name: `${member.displayName || member.username}'s balance`, iconURL: `${member.displayAvatarURL() || client.config.embed.defaultIcon}` })
@@ -11,9 +20,9 @@ module.exports.execute = async (client, interaction, data) => {
         .addFields(
             { name: 'Wallet', value: `:coin: ${memberData.wallet}`, inline: true },
             { name: 'Bank', value: `:coin: ${memberData.bank}`, inline: true },
-            { name: 'Net Worth', value: `:coin: ${memberData.wallet + memberData.bank}`, inline: true }
+            { name: 'Net Worth', value: `:coin: ${memberData.wallet + memberData.bank + invValue}`, inline: true }
         )
-    await interaction.reply({ embeds: [embed] });
+    await interaction.editReply({ embeds: [embed] });
 }
 
 module.exports.help = {
