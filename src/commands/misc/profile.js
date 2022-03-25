2d1c6abf3e6bd349c269ab453b9a71b0c7f04b9f
Siebe Baree <siebe.baree@outlook.com>
2022-03-25 16:53:23 +0100

    Initial commit

---
+++ src/commands/misc/profile.js
@@ -0,0 +1,72 @@
+const { MessageEmbed } = require('discord.js');
+const cooldownsSchema = require("../../database/schemas/cooldowns");
+
+module.exports.execute = async (client, interaction, data) => {
+    const member = interaction.options.getUser('user') || interaction.member;
+    if (member.bot) return interaction.reply({ content: 'That user is a bot. You can only check the profile of a real person.', ephemeral: true });
+    await interaction.deferReply();
+    const memberData = await client.database.fetchGuildUser(interaction.guildId, member.id);
+
+    let invValue = 0;
+    let items = 0;
+    if (memberData.inventory !== undefined) {
+        for (let i = 0; i < memberData.inventory.length; i++) {
+            const item = await client.database.fetchItem(memberData.inventory[i].itemId);
+            invValue += parseInt(item.sellPrice * memberData.inventory[i].quantity);
+            items += memberData.inventory[i].quantity;
+        }
+    }
+
+    let job = memberData.job === "" ? "None" : memberData.job;
+    if (memberData.business !== undefined && memberData.business.name.length > 0) job = "Business CEO";
+
+    let cooldownsAmount = 0;
+    let cooldowns = await cooldownsSchema.find({ guildId: interaction.guildId, userId: member.id });
+    let cooldownStr = "";
+
+    for (let i = 0; i < cooldowns.length; i++) {
+        if (cooldowns[i].expiresOn > parseInt(Date.now() / 1000) && cooldownsAmount <= 10) {
+            cooldownStr += `**${cooldowns[i].command}:** ${client.calc.msToTime(cooldowns[i].expiresOn * 1000 - Date.now())}\n`;
+            cooldownsAmount++;
+        }
+    }
+
+    if (cooldowns.length > 10) {
+        cooldownStr += `and \`${cooldowns.length - cooldownsAmount}\` more cooldowns...`;
+    } else if (cooldownStr === "") {
+        cooldownStr = "There are no cooldowns found for this user.";
+    }
+
+    const embed = new MessageEmbed()
+        .setAuthor({ name: `${member.displayName || member.username}'s profile`, iconURL: `${member.displayAvatarURL() || client.config.embed.defaultIcon}` })
+        .setColor(client.config.embed.color)
+        .setThumbnail(member.displayAvatarURL() || client.config.embed.defaultIcon)
+        .addFields(
+            { name: 'Experience', value: `Coming Soon`, inline: true },
+            { name: 'Level', value: `Coming Soon`, inline: true },
+            { name: 'Balance', value: `:dollar: **Wallet:** :coin: ${memberData.wallet}\n:credit_card: **Bank:** :coin: ${memberData.bank}\n:moneybag: **Net Worth:** :coin: ${memberData.wallet + memberData.bank + invValue}\n:gem: **Inventory Worth:** \`${items} items\` valued at :coin: ${invValue}`, inline: false },
+            { name: 'Misc', value: `:briefcase: **Current Job:** ${job}\n:sparkles: ** Streak:** ${memberData.streak} days\n:no_entry: ** Banned:** ${memberData.banned ? "Yes" : "No"}\n:shield: ** Passive Mode:** ${memberData.passiveMode ? "On" : "Off"}\n`, inline: false },
+            { name: 'Cooldowns', value: `${cooldownStr}`, inline: false }
+        )
+    await interaction.editReply({ embeds: [embed] });
+}
+
+module.exports.help = {
+    name: "profile",
+    description: "Get your or another user's Coinz profile. You can see detailed information here.",
+    options: [
+        {
+            name: 'user',
+            type: 'USER',
+            description: 'Get the profile of another user.',
+            required: false
+        }
+    ],
+    category: "misc",
+    extraFields: [],
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+    ownerOnly: false,
+    cooldown: 3,
+    enabled: true
+}
