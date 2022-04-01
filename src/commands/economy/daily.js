f1efbfacf0b927aa791d4ffd02e7b11fc6c848ed
Siebe Baree <siebe.baree@outlook.com>
2022-04-01 16:41:06 +0200

    Initial commit

---
+++ src/commands/economy/daily.js
@@ -0,0 +1,42 @@
+const guildUserSchema = require('../../database/schemas/guildUsers');
+
+const defaultReward = 15;
+const daysOffset = 3;
+
+module.exports.execute = async (client, interaction, data) => {
+    let streakReward = defaultReward;
+    const lastStreakAgo = parseInt(Date.now() / 1000) - data.guildUser.lastStreak;
+
+    if (data.guildUser.lastStreak === 0 || lastStreakAgo <= 86400 * daysOffset) {
+        streakReward += 10 * (data.guildUser.streak + 1);
+        streakReward = streakReward > 75 ? 75 : streakReward; // cap daily reward at 75
+    } else {
+        await client.tools.addMoney(interaction.guildId, interaction.member.id, defaultReward);
+        return await interaction.reply({ content: `You lost your streak and now get a normal reward of :coin: ${defaultReward}.` });
+    }
+
+    await guildUserSchema.updateOne({ guildId: interaction.guildId, userId: interaction.member.id }, {
+        $inc: {
+            wallet: streakReward,
+            streak: 1
+        },
+        $set: {
+            lastStreak: parseInt(Date.now() / 1000)
+        }
+    });
+
+    await interaction.reply({ content: `You claimed your daily reward and got :coin: ${streakReward}. (:coin: ${streakReward - defaultReward} for ${data.guildUser.streak + 1} day streak.)` });
+}
+
+module.exports.help = {
+    name: "daily",
+    description: "Claim your daily reward.",
+    options: [],
+    category: "economy",
+    extraFields: [],
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+    ownerOnly: false,
+    cooldown: 86400,
+    enabled: true
+}
