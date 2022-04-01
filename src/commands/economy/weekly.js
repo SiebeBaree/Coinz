cde73eaf7408331074fcbd402bbf8c5fc104d8a0
Siebe Baree <siebe.baree@outlook.com>
2022-04-01 16:41:13 +0200

    Initial commit

---
+++ src/commands/economy/weekly.js
@@ -0,0 +1,17 @@
+module.exports.execute = async (client, interaction, data) => {
+    await client.tools.addMoney(interaction.guildId, interaction.member.id, 100);
+    await interaction.reply({ content: `You claimed your weekly reward and got :coin: 100.` });
+}
+
+module.exports.help = {
+    name: "weekly",
+    description: "Claim your weekly reward.",
+    options: [],
+    category: "economy",
+    extraFields: [],
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+    ownerOnly: false,
+    cooldown: 604800,
+    enabled: true
+}
