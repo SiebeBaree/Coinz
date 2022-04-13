f7b76f029faa8244fef321b402c4a9c0d0b0e2df
Siebe Baree <siebe.baree@outlook.com>
2022-04-13 05:11:08 +0200

    Removed disabled properties

--- src/database/schemas/guilds.js
+++ src/database/schemas/guilds.js
@@ -3,7 +3,5 @@ const mongoose = require('mongoose')
 module.exports = mongoose.model("Guilds", new mongoose.Schema({
     guildId: { type: String, require: true },
     isPremium: { type: Boolean, default: false },
-    banned: { type: Boolean, default: false },
-    disabledCommands: [String],
-    disabledChannels: [String]
+    banned: { type: Boolean, default: false }
 }))
