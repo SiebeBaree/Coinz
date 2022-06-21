fee434ab5937363d3e183ab71a1190660094bed5
Siebe Baree <siebe.baree@outlook.com>
2022-06-21 16:01:21 +0200

    Added fetchCompany

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,16 +1,17 @@
-const guildsSchema = require("./schemas/guilds")
-const guildUsersSchema = require("./schemas/guildUsers")
-const shopSchema = require("./schemas/items")
-const stocksSchema = require("./schemas/stocks")
-const usersSchema = require("./schemas/users")
-const cooldownsSchema = require("./schemas/cooldowns")
+const guildsSchema = require("./schemas/guilds");
+const guildUsersSchema = require("./schemas/guildUsers");
+const shopSchema = require("./schemas/items");
+const stocksSchema = require("./schemas/stocks");
+const usersSchema = require("./schemas/users");
+const cooldownsSchema = require("./schemas/cooldowns");
+const companiesSchema = require("./schemas/companies");
 
 module.exports.fetchGuild = async function (guildId) {
     let guildObj = await guildsSchema.findOne({ guildId: guildId });
     if (guildObj) {
         return guildObj;
     } else {
-        guildObj = new guildsSchema({ guildId: guildId })
+        guildObj = new guildsSchema({ guildId: guildId });
         await guildObj.save().catch(err => console.log(err));
         return guildObj;
     }
@@ -21,7 +22,7 @@ module.exports.fetchGuildUser = async function (guildId, userId) {
     if (userObj) {
         return userObj;
     } else {
-        userObj = new guildUsersSchema({ userId: userId, guildId: guildId })
+        userObj = new guildUsersSchema({ userId: userId, guildId: guildId });
         await userObj.save().catch(err => console.log(err));
         return userObj;
     }
@@ -32,7 +33,7 @@ module.exports.fetchUser = async function (userId) {
     if (userObj) {
         return userObj;
     } else {
-        userObj = new usersSchema({ userId: userId })
+        userObj = new usersSchema({ userId: userId });
         await userObj.save().catch(err => console.log(err));
         return userObj;
     }
@@ -51,8 +52,25 @@ module.exports.fetchCooldown = async function (guildId, userId, cmdName) {
     if (cooldownObj) {
         return cooldownObj;
     } else {
-        cooldownObj = new cooldownsSchema({ guildId: guildId, userId: userId, command: cmdName })
+        cooldownObj = new cooldownsSchema({ guildId: guildId, userId: userId, command: cmdName });
         await cooldownObj.save().catch(err => console.log(err));
         return cooldownObj;
+    }
+}
+
+module.exports.fetchCompany = async function (guildId, ownerId, name) {
+    let obj = await companiesSchema.findOne({ guildId: guildId, ownerId: ownerId });
+    if (obj) {
+        return obj;
+    } else {
+        obj = new companiesSchema({
+            guildId: guildId,
+            ownerId: ownerId,
+            name: (name === undefined || name === null) ? "An Unnamed Company" : name,
+            balance: 0,
+            taxRate: 10
+        });
+        await obj.save().catch(err => console.log(err));
+        return obj;
     }
 }
