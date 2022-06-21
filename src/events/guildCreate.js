1261b8dba68f88aed919cc30f867515410bacae0
Siebe Baree <siebe.baree@outlook.com>
2022-06-21 16:03:20 +0200

    Initial commit

---
+++ src/events/guildCreate.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+module.exports = async (client, guild) => {
+    if (guild.available) {
+        const rawData = fs.readFileSync('src/data/events/guildJoinLeave.json');
+        let joinLeaveData = JSON.parse(rawData);
+        let now = new Date();
+
+        if (joinLeaveData[`${guild.id}`] === undefined) {
+            joinLeaveData[`${guild.id}`] = {
+                "name": `${guild.name}`,
+                "members": `${guild.memberCount}`,
+                "logs": [
+                    `JOIN @ ${now.getDate()}/${now.getMonth()}/${now.getFullYear()} - ${now.getHours()}:${now.getMinutes()}`
+                ]
+            }
+        } else {
+            joinLeaveData[`${guild.id}`]["name"] = `${guild.name}`;
+            joinLeaveData[`${guild.id}`]["members"] = `${guild.memberCount}`;
+            joinLeaveData[`${guild.id}`]["logs"].push(`JOIN @ ${now.getDate()}/${now.getMonth()}/${now.getFullYear()} - ${now.getHours()}:${now.getMinutes()}`);
+        }
+
+        fs.writeFile('src/data/events/guildJoinLeave.json', JSON.stringify(joinLeaveData, null, 4), (err) => {
+            if (err) {
+                client.logger.error(err.message);
+            } else {
+                client.logger.event(`${guild.name} has invited Coinz! (ID: ${guild.id}, Members: ${guild.memberCount})`);
+            }
+        })
+    }
+}
