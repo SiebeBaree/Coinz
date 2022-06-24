53a7fbfb7cbe96170d8e9cf906f02533e4447991
Siebe Baree <siebe.baree@outlook.com>
2022-06-24 10:17:02 +0200

    Uncommented Code

--- website/webApp/__init__.py
+++ website/webApp/__init__.py
@@ -9,12 +9,12 @@ def create_app():
     app.secret_key = config.get("APP_SECRET_KEY")
 
     from webApp.main.routes import main
-    # from webApp.login.routes import login
-    # from webApp.dashboard.routes import dashboard
+    from webApp.login.routes import login
+    from webApp.dashboard.routes import dashboard
     from webApp.errors.handlers import errors
     app.register_blueprint(main)
-    # app.register_blueprint(login)
-    # app.register_blueprint(dashboard, url_prefix="/dashboard")
+    app.register_blueprint(login)
+    app.register_blueprint(dashboard, url_prefix="/dashboard")
     app.register_blueprint(errors)
 
     return app
