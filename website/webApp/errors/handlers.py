89403f1e8c817e6d10d8a9c6515f5572a547d1fd
Siebe Baree <siebe.baree@outlook.com>
2022-06-24 10:16:35 +0200

    Small changes

--- website/webApp/errors/handlers.py
+++ website/webApp/errors/handlers.py
@@ -5,14 +5,14 @@ errors = Blueprint('errors', __name__)
 
 @errors.app_errorhandler(403)
 def error_403(error):
-    return render_template('errors/403.html'), 403
+    return render_template('errors/403.html', title="403 - Forbidden"), 403
 
 
 @errors.app_errorhandler(404)
 def error_404(error):
-    return render_template('errors/404.html'), 404
+    return render_template('errors/404.html', title="404 - Not Found"), 404
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
-    return render_template('errors/500.html'), 500
+    return render_template('errors/500.html', title="500 - Server Error"), 500
