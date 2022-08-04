3edd218676bba912ec1fbaad84f865483dcd9519
Siebe Baree <siebe.baree@outlook.com>
2022-08-04 12:31:09 +0200

    Reworked command

--- src/commands/economy/fish.js
+++ src/commands/economy/fish.js
@@ -1,106 +1,135 @@
-const { MessageEmbed } = require('discord.js');
+const Command = require('../../structures/Command.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors } = require('discord.js');
+const lootTable = require('../../assets/lootTables/fish.json').loot;
 
-const lootTable = {
-    "fail": {
-        "chance": 30,
-        "loot": []
-    },
-    "common": {
-        "chance": 40,
-        "loot": ["cod", "salmon", "tuna", "sardine", "trash", "shoe", "socks"]
-    },
-    "uncommon": {
-        "chance": 22,
-        "loot": ["jellyfish", "catfish", "lobster", "goldfish"]
-    },
-    "rare": {
-        "chance": 6.9,
-        "loot": ["crab", "octopus", "sea_turtle", "squid"]
-    },
-    "super_rare": {
-        "chance": 1,
-        "loot": ["blobfish", "starfish", "anglerfish"]
-    },
-    "legendary": {
-        "chance": 0.1,
-        "loot": ["diamond", "crown"]
+class Fish extends Command {
+    info = {
+        name: "fish",
+        description: "Try and catch some fish that you can sell for money.",
+        options: [],
+        category: "economy",
+        extraFields: [],
+        memberPermissions: [],
+        botPermissions: [],
+        cooldown: 900,
+        enabled: true
+    };
+
+    constructor(...args) {
+        super(...args);
+    }
+
+    async run(interaction, data) {
+        await interaction.deferReply();
+
+        if (!await bot.tools.checkItem(data.user.inventory, "fishing_rod")) {
+            await bot.cooldown.removeCooldown(interaction.member.id, this.info.name);
+            return await interaction.editReply({ content: "You need a fishing rod to use this command. Use `/shop buy item-id:fishing_rod` to buy a fishing rod." });
+        }
+
+        if (bot.tools.randomNumber(1, 100) <= 5) {
+            await bot.cooldown.removeCooldown(interaction.member.id, this.info.name);
+            await bot.tools.takeItem(interaction.member.id, "fishing_rod", data.user.inventory, 1);
+            return await interaction.editReply({ content: "Oh No! Your Fishing Rod broke... You have to buy a new fishing rod. Use `/shop buy item-id:fishing_rod` to buy a fishing rod." });
+        }
+
+        let status = "too soon";
+        let finishedCommand = false;
+
+        let embed = new EmbedBuilder()
+            .setAuthor({ name: `Fishing with ${interaction.member.displayName}`, iconURL: interaction.member.displayAvatarURL() })
+            .setColor(bot.config.embed.color)
+            .setDescription(`Press the button below to fish. **ONLY PRESS IT WHEN THE BUTTON IS GREEN!**`)
+
+        const interactionMessage = await interaction.editReply({ embeds: [embed], components: [this.getRow(status)], fetchReply: true });
+        const collector = bot.tools.createMessageComponentCollector(interactionMessage, interaction, { time: 15000 });
+
+        collector.on('collect', async (interactionCollector) => {
+            await interactionCollector.deferUpdate();
+
+            if (!finishedCommand) {
+                finishedCommand = true;
+
+                if (interactionCollector.customId.startsWith('fish_')) {
+                    if (status === "too soon") {
+                        embed.setColor(Colors.Red);
+                        embed.setDescription("You were too soon to catch some fish. Don't press the button until it's green!");
+                    } else if (status === "ready") {
+                        const loot = bot.tools.getRandomLoot(lootTable, 1, 5);
+
+                        let mappedLoot = [];
+                        for (let obj of loot) {
+                            const index = mappedLoot.findIndex(item => item.itemId === obj.itemId);
+                            if (index === -1) {
+                                obj.quantity = 1;
+                                mappedLoot.push(obj);
+                            } else {
+                                mappedLoot[index].quantity++;
+                            }
+                        }
+
+                        let lootText = "";
+                        let totalPrice = 0;
+                        for (let i = 0; i < mappedLoot.length; i++) {
+                            lootText += `${mappedLoot[i].quantity}x ${mappedLoot[i].name} <:${mappedLoot[i].itemId}:${mappedLoot[i].emoteId}> ― :coin: ${parseInt(mappedLoot[i].sellPrice * mappedLoot[i].quantity)}\n`;
+                            totalPrice += parseInt(mappedLoot[i].sellPrice * mappedLoot[i].quantity);
+                        }
+
+                        if (lootText.length > 0 && totalPrice > 0) {
+                            embed.addFields({ name: "Loot", value: lootText, inline: false });
+                            embed.setColor(Colors.Green);
+                            embed.setDescription(`You caught some fish and sold it for :coin: ${totalPrice}.`);
+                            await bot.tools.addMoney(interaction.member.id, totalPrice);
+                        }
+                    } else {
+                        embed.setColor(Colors.Red);
+                        embed.setDescription("You were too late to catch some fish. Be quicker next time!");
+                    }
+
+                    await interaction.editReply({ embeds: [embed], components: [this.getRow(status, true)] });
+                }
+            }
+        });
+
+        collector.on('end', async (interactionCollector) => {
+            if (!finishedCommand) {
+                finishedCommand = true;
+
+                embed.setColor(Colors.Red);
+                embed.setDescription("You were too late to catch some fish. Be quicker next time!");
+                await interaction.editReply({ embeds: [embed], components: [this.getRow("too late", true)] });
+            }
+        });
+
+        const seconds = bot.tools.randomNumber(5, 10);
+        for (let i = 0; i < seconds; i++) await bot.tools.timeout(1000);
+
+        if (!finishedCommand) {
+            status = "ready";
+            await interaction.editReply({ components: [this.getRow(status)] });
+        } else {
+            await bot.tools.timeout(1500);
+            if (!finishedCommand) {
+                finishedCommand = true;
+
+                status = "too late";
+                embed.setColor(Colors.Red);
+                embed.setDescription("You were too late to catch some fish. Be quicker next time!");
+                await interaction.editReply({ embeds: [embed], components: [this.getRow("too late", true)] });
+            }
+        }
+    }
+
+    getRow(status, disabled = false) {
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId("fish_end")
+                .setLabel("Reel in Fishing Rod")
+                .setStyle(status === "too soon" ? ButtonStyle.Secondary : ((status === "ready") ? ButtonStyle.Success : ButtonStyle.Danger))
+                .setDisabled(disabled || status === "too late")
+        );
+        return row;
     }
 }
 
-function randomKey(client, lootTable) {
-    const keys = Object.keys(lootTable);
-    let randomNumber = client.tools.randomNumber(1, 1000);
-    randomNumber = randomNumber / 10.0;
-
-    let totalChance = 0;
-    for (let i = 0; i < keys.length; i++) {
-        if (randomNumber < totalChance + lootTable[keys[i]].chance) return keys[i];
-        totalChance += lootTable[keys[i]].chance;
-    }
-    return keys[0];
-}
-
-function getLoot(client, key, lootTable) {
-    let allLoot = lootTable[key].loot;
-    if (key === "legendary") return allLoot[client.tools.randomNumber(0, allLoot.length - 1)];
-
-    let itemCount = client.tools.randomNumber(1, 5);
-    let loot = [];
-    for (let i = 0; i < itemCount; i++) {
-        loot.push(allLoot[client.tools.randomNumber(0, allLoot.length - 1)]);
-    }
-
-    // To make sure at least 1 item is returned.
-    return loot === [] ? [allLoot[0]] : loot;
-}
-
-module.exports.execute = async (client, interaction, data) => {
-    if (!await client.tools.userHasItem(data.guildUser.inventory, "fishing_rod")) {
-        await client.cooldown.removeCooldown(interaction.guildId, interaction.member.id, data.cmd.help.name);
-        return await interaction.reply({ content: "You need a fishing rod to use this command. Use `/shop buy item_id:fishing_rod` to buy a fishing rod.", ephemeral: true });
-    }
-
-    if (client.tools.randomNumber(1, 100) <= 5) {
-        await client.cooldown.removeCooldown(interaction.guildId, interaction.member.id, data.cmd.help.name);
-        await client.tools.takeItem(interaction, data, "fishing_rod", 1);
-        return await interaction.reply({ content: "Oh No! Your Fishing Rod broke... You have to buy a new fishing rod. Use `/shop buy item_id:fishing_rod` to buy a fishing rod." });
-    }
-
-    let key = randomKey(client, lootTable);
-    if (key === "fail") return await interaction.reply({ content: "You haven't caught any fish today :(" });
-
-    await interaction.deferReply();
-    let loot = getLoot(client, key, lootTable);
-    let lootText = "";
-
-    let mappedLoot = loot.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
-    let keys = Object.keys(mappedLoot);
-
-    for (let i = 0; i < keys.length; i++) {
-        const item = await client.database.fetchItem(keys[i]);
-        lootText += `${mappedLoot[keys[i]]}x ${item.name} <:${item.itemId}:${item.emoteId}>\n`;
-        await client.tools.giveItem(interaction, data, item.itemId, mappedLoot[keys[i]]);
-    }
-
-    if (lootText === "") lootText = "They got away :(";
-
-    const embed = new MessageEmbed()
-        .setTitle("Fish")
-        .setColor(client.config.embed.color)
-        .setDescription("You caught some fish!")
-        .addField('Loot', `${lootText}`, false)
-    await interaction.editReply({ embeds: [embed] });
-}
-
-module.exports.help = {
-    name: "fish",
-    description: "Try and catch some fish that you can sell for money.",
-    options: [],
-    category: "economy",
-    extraFields: [],
-    memberPermissions: [],
-    botPermissions: [],
-    ownerOnly: false,
-    cooldown: 1800,
-    enabled: true
-}
+module.exports = Fish;
