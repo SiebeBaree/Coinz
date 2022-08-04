9d5a74c1fdcab03a656dca14754caf92f812ab7d
Siebe Baree <siebe.baree@outlook.com>
2022-08-04 12:33:00 +0200

    Updated to discord.js v14

--- src/commands/misc/info.js
+++ src/commands/misc/info.js
@@ -1,44 +1,52 @@
-const { MessageEmbed } = require('discord.js');
-var { dependencies } = require('../../../package.json');
+const Command = require('../../structures/Command.js');
+const { dependencies, version } = require('../../../package.json');
+const { EmbedBuilder } = require('discord.js');
 
-module.exports.execute = async (client, interaction, data) => {
-    await interaction.deferReply();
-    const usedMemory = process.memoryUsage().heapUsed / 1024 / 1024;
+class Info extends Command {
+    info = {
+        name: "info",
+        description: "Get some information about Coinz.",
+        options: [],
+        category: "misc",
+        extraFields: [],
+        memberPermissions: [],
+        botPermissions: [],
+        cooldown: 0,
+        enabled: true
+    };
 
-    const promises = [
-        client.shard.fetchClientValues('guilds.cache.size'),
-        client.shard.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)),
-    ];
+    constructor(...args) {
+        super(...args);
+    }
 
-    Promise.all(promises)
-        .then(results => {
-            const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
-            const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+    async run(interaction, data) {
+        await interaction.deferReply();
+        const usedMemory = process.memoryUsage().heapUsed / 1024 / 1024;
 
-            const newEmbed = new MessageEmbed()
-                .setAuthor({ name: `Bot Statistics`, iconURL: `${client.user.avatarURL() || client.config.embed.defaultIcon}` })
-                .setColor(client.config.embed.color)
-                .setFooter({ text: client.config.embed.footer })
-                .setTimestamp()
-                .setDescription('**If you like this bot, maybe consider [donating](https://coinzbot.xyz/donate).**')
-                .addFields(
-                    { name: 'Info', value: `:man_technologist: **Developer:** \`Siebe#9999\`\n:globe_with_meridians: **Website: [coinzbot.xyz](${client.config.website})**\n:beginner: **Official Server: [discord.gg/asnZQwc6kW](https://discord.gg/asnZQwc6kW)**\n:books: **Library:** \`discord.js${dependencies["discord.js"]}\``, inline: true },
-                    { name: 'Statistics', value: `:video_game: **Commands:** \`${client.commands.size}\`\n:spider_web: **Shard:** \`${interaction.guild.shardId + 1}/${client.shard.count}\`\n:white_check_mark: **Uptime:** \`${client.calc.msToTime(client.uptime)}\`\n:film_frames: **Memory Usage:** \`${Math.round(usedMemory * 100) / 100}\` MB\n:bust_in_silhouette: **Users:** \`${totalMembers}\`\n:printer: **Servers:** \`${totalGuilds}\``, inline: true },
-                    { name: 'Disclamer', value: `Icons from the shop are from [icons8](https://icons8.com).`, inline: false }
-                )
-            return interaction.editReply({ embeds: [newEmbed] });
-        }).catch();
+        const promises = [
+            bot.shard.fetchClientValues('guilds.cache.size'),
+            bot.shard.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)),
+        ];
+
+        Promise.all(promises)
+            .then(results => {
+                const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+                const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+
+                const newEmbed = new EmbedBuilder()
+                    .setAuthor({ name: `Bot Statistics`, iconURL: `${bot.user.avatarURL() || bot.config.embed.defaultIcon}` })
+                    .setColor(bot.config.embed.color)
+                    .setFooter({ text: bot.config.embed.footer })
+                    .setTimestamp()
+                    .setDescription('**If you like this bot, maybe consider [donating](https://coinzbot.xyz/donate).**')
+                    .addFields(
+                        { name: 'Info', value: `:man_technologist: **Developer:** \`Siebe#9999\`\n:globe_with_meridians: **Website: [coinzbot.xyz](${bot.config.website})**\n:beginner: **Official Server: [discord.gg/asnZQwc6kW](https://discord.gg/asnZQwc6kW)**\n:books: **Library:** \`discord.js${dependencies["discord.js"]}\`\n:star: **Version:** \`${version}\``, inline: true },
+                        { name: 'Statistics', value: `:video_game: **Commands:** \`${bot.commands.size}\`\n:spider_web: **Shard:** \`${interaction.guild.shardId + 1}/${bot.shard.count}\`\n:white_check_mark: **Uptime:** \`${bot.tools.msToTime(bot.uptime)}\`\n:film_frames: **Memory Usage:** \`${Math.round(usedMemory * 100) / 100} MB\`\n:bust_in_silhouette: **Users:** \`${totalMembers}\`\n:printer: **Servers:** \`${totalGuilds}\``, inline: true },
+                        { name: 'Disclamer', value: `Icons from the shop are from [icons8](https://icons8.com).`, inline: false }
+                    )
+                return interaction.editReply({ embeds: [newEmbed] });
+            }).catch();
+    }
 }
 
-module.exports.help = {
-    name: "info",
-    description: "Get some information about Coinz.",
-    options: [],
-    category: "misc",
-    extraFields: [],
-    memberPermissions: [],
-    botPermissions: [],
-    ownerOnly: false,
-    cooldown: 3,
-    enabled: true
-}
+module.exports = Info;
--- src/commands/misc/invite.js
+++ src/commands/misc/invite.js
@@ -1,16 +1,25 @@
-module.exports.execute = async (client, interaction, data) => {
-    await interaction.reply({ content: `**Join our Official Support Discord Server:** discord.gg/asnZQwc6kW` });
+const Command = require('../../structures/Command.js');
+
+class Invite extends Command {
+    info = {
+        name: "invite",
+        description: "Get a invite to our Official Support Discord Server",
+        options: [],
+        category: "misc",
+        extraFields: [],
+        memberPermissions: [],
+        botPermissions: [],
+        cooldown: 0,
+        enabled: true
+    };
+
+    constructor(...args) {
+        super(...args);
+    }
+
+    async run(interaction, data) {
+        await interaction.reply({ content: `:pushpin: **Invite Coinz yourself:** [**Click Here**](https://discord.com/oauth2/authorize?client_id=938771676433362955&permissions=313344&scope=bot%20applications.commands)\n:wave: **Join our Official Support Discord Server:** discord.gg/asnZQwc6kW` });
+    }
 }
 
-module.exports.help = {
-    name: "invite",
-    description: "Get a invite to our Official Support Discord Server",
-    options: [],
-    category: "misc",
-    extraFields: [],
-    memberPermissions: [],
-    botPermissions: [],
-    ownerOnly: false,
-    cooldown: 3,
-    enabled: true
-}
+module.exports = Invite;
