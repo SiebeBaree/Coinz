b5435fc524ba6468d3939ba91688f6351dcfbad9
Siebe Baree <siebe.baree@outlook.com>
2022-08-04 12:33:43 +0200

    Removed error handlers

--- website/webApp/__init__.py
+++ website/webApp/__init__.py
@@ -11,10 +11,8 @@ def create_app():
     from webApp.main.routes import main
     from webApp.login.routes import login
     from webApp.dashboard.routes import dashboard
-    from webApp.errors.handlers import errors
     app.register_blueprint(main)
     app.register_blueprint(login)
     app.register_blueprint(dashboard, url_prefix="/dashboard")
-    app.register_blueprint(errors)
 
     return app
