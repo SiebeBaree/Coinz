9823f1387bb55fcf20d95f94f1a6e02f769d8d01
Siebe Baree <siebe.baree@outlook.com>
2022-08-04 11:51:36 +0200

    Updated to global cooldown system

--- src/tools/cooldown.js
+++ src/tools/cooldown.js
@@ -1,23 +1,23 @@
 const cooldownSchema = require('../database/schemas/cooldowns');
 
 
-module.exports.isOnCooldown = async (client, guildId, userId, cmdName) => {
-    let cooldownData = await client.database.fetchCooldown(guildId, userId, cmdName);
+module.exports.isOnCooldown = async (client, userId, cmdName) => {
+    let cooldownData = await client.database.fetchCooldown(userId, cmdName);
     if (cooldownData.expiresOn && (cooldownData.expiresOn > parseInt(Date.now() / 1000))) return true;
     else return false;
 };
 
-module.exports.removeCooldown = async (guildId, userId, cmdName) => {
-    await cooldownSchema.deleteOne({ guildId: guildId, userId: userId, command: cmdName });
+module.exports.removeCooldown = async (userId, cmdName) => {
+    await cooldownSchema.deleteOne({ userId: userId, command: cmdName });
 };
 
-module.exports.setCooldown = async (guildId, userId, cmdName, cooldown = 3) => {
-    await cooldownSchema.updateOne({ guildId: guildId, userId: userId, command: cmdName }, {
+module.exports.setCooldown = async (userId, cmdName, cooldown = 3) => {
+    await cooldownSchema.updateOne({ userId: userId, command: cmdName }, {
         expiresOn: parseInt(Date.now() / 1000) + cooldown
     });
 };
 
-module.exports.getCooldown = async (client, guildId, userId, cmdName) => {
-    let cooldownData = await client.database.fetchCooldown(guildId, userId, cmdName);
+module.exports.getCooldown = async (client, userId, cmdName) => {
+    let cooldownData = await client.database.fetchCooldown(userId, cmdName);
     return cooldownData.expiresOn - parseInt(Date.now() / 1000);
 };
