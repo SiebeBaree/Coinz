b5136035f19a06ee9de19956add4c90fdda50386
Siebe Baree <siebe.baree@outlook.com>
2022-08-04 12:35:02 +0200

    Added new routes

--- website/webApp/main/routes.py
+++ website/webApp/main/routes.py
@@ -1,31 +1,48 @@
 from webApp import config
-from webApp.utils import is_authorized
-from flask import render_template, session, redirect, Blueprint, abort
+from flask import render_template, session, redirect, Blueprint
 
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 def index():
-    logged_in = is_authorized()  # "token" in session
+    logged_in = "token" in session
     user = session.get('user')
     return render_template("index.html", logged_in=logged_in, user=user)
 
 
-@main.route("/donate")
-def donate():
-    return redirect("https://ko-fi.com/coinz")
+@main.route("/terms-of-service")
+@main.route("/tos")
+def terms_of_service():
+    logged_in = "token" in session
+    user = session.get('user')
+    return render_template("terms_of_service.html", logged_in=logged_in, user=user)
 
 
-@main.route("/discord")
-def discord():
-    return redirect("https://discord.gg/asnZQwc6kW")
+@main.route("/privacy-policy")
+def privacy_policy():
+    logged_in = "token" in session
+    user = session.get('user')
+    return render_template("privacy_policy.html", logged_in=logged_in, user=user)
+
+
+@main.route("/faq")
+def faq():
+    logged_in = "token" in session
+    user = session.get('user')
+    return render_template("faq.html", logged_in=logged_in, user=user)
+
+
+@main.route("/commands")
+def commands():
+    logged_in = "token" in session
+    user = session.get('user')
+    return render_template("commands.html", logged_in=logged_in, user=user)
 
 
 @main.route("/invite")
 @main.route("/invite/<guild_id>")
 def invite(guild_id=None):
-    # session.pop("guilds", None)
     url = f"https://discord.com/api/oauth2/authorize?client_id={config.get('DISCORD_CLIENT_ID')}&permissions=313344&scope=bot%20applications.commands"
 
     if guild_id is not None:
@@ -34,19 +51,23 @@ def invite(guild_id=None):
     return redirect(url)
 
 
-@main.route("/terms-of-service")
-@main.route("/tos")
-def terms_of_service():
-    logged_in = "token" in session
-    return render_template("terms_of_service.html", logged_in=logged_in)
+@main.route("/donate")
+@main.route("/premium")
+def donate():
+    return redirect("https://patreon.com/join/coinz_bot")
 
 
-@main.route("/privacy-policy")
-def privacy_policy():
-    logged_in = "token" in session
-    return render_template("privacy_policy.html", logged_in=logged_in)
+@main.route("/discord")
+@main.route("/support")
+def discord():
+    return redirect("https://discord.gg/asnZQwc6kW")
 
 
-@main.route("/vote")
-def vote():
-    return abort(404)
+@main.route("/ban-appeal")
+def ban_appeal():
+    return redirect("https://google.com/")
+
+
+@main.route("/report-user")
+def report_user():
+    return redirect("https://google.com/")
