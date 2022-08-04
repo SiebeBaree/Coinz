2e36826bc0878a98aa15e49fa3b7184292530e44
Siebe Baree <siebe.baree@outlook.com>
2022-08-04 12:30:58 +0200

    Reworked command

--- src/commands/economy/crime.js
+++ src/commands/economy/crime.js
@@ -1,138 +1,86 @@
-const { MessageEmbed } = require('discord.js');
+const Command = require('../../structures/Command.js');
+const lootTable = require('../../assets/lootTables/crime.json');
+const { EmbedBuilder } = require('discord.js');
 
-// Placeholders for sentences:
-// %money% ==> The money the user got from crime
-// %crimename% ==> The name of the crime the user comitted
-
-const crimes = [
-    {
-        name: "House Robbery",
-        icon: "",
-        index: 1,
-        sentences: [
-            "You found %money% during your criminal activity"
-        ],
-        lootTable: {
-            "fail": {
-                "chance": 60,
-                "loot": []
-            },
-            "common": {
-                "chance": 20,
-                "loot": ["shoe", "doll", "mirror", "lamp"]
-            },
-            "uncommon": {
-                "chance": 13,
-                "loot": ["hunting_rifle", "padlock"]
-            },
-            "rare": {
-                "chance": 5.9,
-                "loot": ["phone", "playstation"]
-            },
-            "super_rare": {
-                "chance": 1,
-                "loot": ["gold", "bomb"]
-            },
-            "legendary": {
-                "chance": 0.1,
-                "loot": ["diamond", "trophy"]
-            }
-        },
+class Crime extends Command {
+    info = {
+        name: "crime",
+        description: "Commit a crime and get the chance to become rich...",
+        options: [],
+        category: "economy",
+        extraFields: [],
+        memberPermissions: [],
+        botPermissions: [],
+        cooldown: 900,
         enabled: true
-    },
-    {
-        name: "Fraud",
-        icon: "",
-        index: 2,
-        lootTable: {},
-        enabled: false
-    },
-    {
-        name: "Bank Robbery",
-        icon: "",
-        index: 3,
-        lootTable: {},
-        enabled: false
-    }
-]
+    };
 
-function randomKey(client, lootTable) {
-    const keys = Object.keys(lootTable);
-    let randomNumber = client.tools.randomNumber(1, 1000);
-    randomNumber = randomNumber / 10.0;
-
-    let totalChance = 0;
-    for (let i = 0; i < keys.length; i++) {
-        if (randomNumber < totalChance + lootTable[keys[i]].chance) return keys[i];
-        totalChance += lootTable[keys[i]].chance;
+    constructor(...args) {
+        super(...args);
     }
-    return keys[0];
+
+    async run(interaction, data) {
+        await interaction.deferReply();
+        let key = this.randomKey();
+
+        if (key === "fail") {
+            const moneyLost = bot.tools.randomNumber(100, 500);
+            await bot.tools.takeMoney(interaction.member.id, moneyLost, true);
+            return await interaction.editReply({ content: `You got caught during your criminal activities. You had to go to jail but already paid bail of :coin: ${moneyLost}.` });
+        }
+
+        let loot = this.getLoot(key);
+        let mappedLoot = loot.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
+        let keys = Object.keys(mappedLoot);
+
+        let lootText = "";
+        let totalPrice = 0;
+        for (let i = 0; i < keys.length; i++) {
+            const item = await bot.database.fetchItem(keys[i]);
+            totalPrice += item.sellPrice;
+            lootText += `${mappedLoot[keys[i]]}x ${item.name} <:${item.itemId}:${item.emoteId}>\n`;
+            await bot.tools.addItem(interaction.member.id, item.itemId, mappedLoot[keys[i]], data.user.inventory);
+        }
+
+        const moneyWon = bot.tools.randomNumber(1, 300);
+        await bot.tools.addMoney(interaction.member.id, moneyWon);
+
+        const embed = new EmbedBuilder()
+            .setTitle("Crime")
+            .setColor(bot.config.embed.color)
+            .setDescription(`:detective: **You robbed a house and stole some items.**\n:moneybag: **You can sell the items for :coin: ${totalPrice}.**\n:gem: **Sell items using** \`/shop sell <item-id> [amount]\``)
+            .addFields(
+                { name: 'Loot', value: `${lootText || "You almost got caught! But now you don't have any loot :("}`, inline: false }
+            )
+        await interaction.editReply({ embeds: [embed] });
+    }
+
+    randomKey() {
+        const keys = Object.keys(lootTable);
+        let randomNumber = bot.tools.randomNumber(1, 1000);
+        randomNumber = randomNumber / 10.0;
+
+        let totalChance = 0;
+        for (let i = 0; i < keys.length; i++) {
+            if (randomNumber < totalChance + lootTable[keys[i]].chance) return keys[i];
+            totalChance += lootTable[keys[i]].chance;
+        }
+        return keys[0];
+    }
+
+    getLoot(key) {
+        let allLoot = lootTable[key].loot;
+        if (key === "legendary") return allLoot[bot.tools.randomNumber(0, allLoot.length - 1)];
+
+        let itemCount = bot.tools.randomNumber(2, 6);
+        let loot = [];
+        for (let i = 0; i < itemCount; i++) {
+            loot.push(allLoot[bot.tools.randomNumber(0, allLoot.length - 1)]);
+        }
+
+        // To make sure at least 1 item is returned.
+        return loot === [] ? [allLoot[0]] : loot;
+    }
 }
 
-function getLoot(client, key, lootTable) {
-    let allLoot = lootTable[key].loot;
-    if (key === "legendary") return allLoot[client.tools.randomNumber(0, allLoot.length - 1)];
-
-    let itemCount = client.tools.randomNumber(3, 10);
-    let loot = [];
-    for (let i = 0; i < itemCount; i++) {
-        loot.push(allLoot[client.tools.randomNumber(0, allLoot.length - 1)]);
-    }
-
-    // To make sure at least 1 item is returned.
-    return loot === [] ? [allLoot[0]] : loot;
-}
-
-module.exports.execute = async (client, interaction, data) => {
-    let randomCrime = crimes[0]; // Soon => crimes[client.tools.randomNumber(0, crimes.length)];
-    let key = randomKey(client, randomCrime.lootTable);
-
-    if (key === "fail") {
-        const moneyLost = client.tools.randomNumber(50, 1200);
-        await client.tools.removeMoney(interaction.guildId, interaction.member.id, moneyLost, true);
-        return await interaction.reply({ content: `You got caught during your criminal activities. You had to go to jail but already paid bail of :coin: ${moneyLost}.` });
-    }
-
-    await interaction.deferReply();
-    let loot = getLoot(client, key, randomCrime.lootTable);
-    let lootText = "";
-
-    let mappedLoot = loot.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
-    let keys = Object.keys(mappedLoot);
-
-    for (let i = 0; i < keys.length; i++) {
-        const item = await client.database.fetchItem(keys[i]);
-        lootText += `${mappedLoot[keys[i]]}x ${item.name} <:${item.itemId}:${item.emoteId}>\n`;
-        await client.tools.giveItem(interaction, data, item.itemId, mappedLoot[keys[i]]);
-    }
-
-    const moneyWon = client.tools.randomNumber(1, 300);
-    if (lootText === "") lootText = "You almost got caught! But now you don't have any loot :(";
-    let sentence = randomCrime.sentences[0, randomCrime.sentences.length - 1];
-
-    // Adding placeholders
-    sentence = sentence.replace("%money%", `:coin: ${moneyWon}`);
-    sentence = sentence.replace("%crimename%", randomCrime.name);
-
-    await client.tools.addMoney(interaction.guildId, interaction.member.id, moneyWon);
-
-    const embed = new MessageEmbed()
-        .setTitle("Crime")
-        .setColor(client.config.embed.color)
-        .setDescription(sentence)
-        .addField('Loot', `${lootText}`, false)
-    await interaction.editReply({ embeds: [embed] });
-}
-
-module.exports.help = {
-    name: "crime",
-    description: "Commit a crime and get the chance to become rich...",
-    options: [],
-    category: "economy",
-    extraFields: [],
-    memberPermissions: [],
-    botPermissions: [],
-    ownerOnly: false,
-    cooldown: 3600,
-    enabled: true
-}
+module.exports = Crime;
