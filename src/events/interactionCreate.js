cc475569e44660e1e016aa83ec86d22a117c5929
Siebe Baree <siebe.baree@outlook.com>
2022-08-07 14:01:23 +0200

    Reduced runtime

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -6,10 +6,8 @@ module.exports = class extends Event {
     }
 
     async run(interaction) {
-        let { guild, member } = interaction;
-
-        if (!guild) return; // Return if dms or group chat
-        if (member.bot) return; // Return if member is bot
+        if (!interaction.guild) return; // Return if dms or group chat
+        if (interaction.member.bot) return; // Return if member is bot
 
         //Checking if the message is a command
         const cmd = this.commands.get(interaction.commandName);
@@ -17,16 +15,14 @@ module.exports = class extends Event {
         //If it isn't a command then return
         if (!cmd) return;
 
-        const userData = await this.database.fetchMember(member.id);
-        const guildData = await this.database.fetchGuild(guild.id);
+        if (cmd.memberRequired === true || cmd.guildRequired === true) await interaction.deferReply();
+        const memberData = cmd.memberRequired === true ? await this.database.fetchMember(interaction.member.id) : undefined;
+        const guildData = cmd.guildRequired === true ? await this.database.fetchGuild(interaction.guild.id) : undefined;
 
-        // Checking if the user or guild is banned.
-        if (userData.banned === true || guildData.banned === true) return;
-
-        if (await cmd.cool(cmd.info.name, member, cmd.info.cooldown)) {
-            return await interaction.reply({ content: `:x: You have to wait ${this.tools.msToTime(await this.cooldown.getCooldown(member.id, cmd.info.name) * 1000)} to use this command again.`, ephemeral: true });
+        if (await cmd.cool(cmd.info.name, interaction.member, cmd.info.cooldown)) {
+            return await interaction.editReply({ content: `:x: You have to wait ${this.tools.msToTime(await this.cooldown.getCooldown(interaction.member.id, cmd.info.name) * 1000)} to use this command again.` });
         }
 
-        await cmd.run(interaction, { user: userData, guild: guildData });
+        await cmd.run(interaction, { user: memberData, guild: guildData });
     }
 };
