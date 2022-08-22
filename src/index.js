d6643e56dcbfcc042a0f4a3c9c33774fbc1f2b0d
Siebe Baree <siebe.baree@outlook.com>
2022-08-22 09:07:01 +0200

    Added shard support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 const { ShardingManager } = require('discord.js');
+const loggerStruct = require('./structures/Logger');
 
-const manager = new ShardingManager('./src/bot.js', { totalShards: 1, token: process.env.DISCORD_TOKEN });
+const manager = new ShardingManager('./src/bot.js', { token: process.env.DISCORD_TOKEN });
+const logger = new loggerStruct(manager);
+
+manager.on('shardCreate', shard => logger.load(`Loading shard ${shard.id}.`));
 manager.spawn();
