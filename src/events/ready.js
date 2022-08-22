84405d580739a4b9ba1fd30e84ffe44aa1fe7e1a
Siebe Baree <siebe.baree@outlook.com>
2022-08-22 09:06:28 +0200

    Added shard support

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,5 @@
 const Event = require('../structures/Event.js');
 const { connect } = require('mongoose');
-const { Poster } = require('dbots');
 require('dotenv').config();
 
 module.exports = class extends Event {
@@ -38,28 +37,6 @@ module.exports = class extends Event {
         });
 
         await this.application?.commands.set(data);  // Used to set slash commands globally [Can take several hours to update.]
-        this.logger.ready(`Logged in as ${this.user.tag}`);
-
-        // Make sure the client has logged in before initializing a poster.
-        // const poster = new Poster({
-        //     this,
-        //     apiKeys: {
-        //         discordbotlist: process.env.API_DISCORDBOTLIST,
-        //         discordbotsgg: {
-        //             token: process.env.API_DISCORDBOTSGG,
-        //             userAgent: {
-        //                 clientID: '938771676433362955',
-        //                 library: 'discord.js'
-        //             }
-        //         },
-        //         discords: process.env.API_DISCORDS,
-        //         disforge: process.env.API_DISFORGE,
-        //         topgg: process.env.API_TOPGG
-        //     },
-        //     clientLibrary: 'discord.js'
-        // })
-
-        // poster.startInterval(); // Starts an interval thats posts to all services every 30 minutes.
-        this.logger.ready("Poster is online.");
+        this.logger.ready(`Shard ${this.shard.ids[0]} loaded.`);
     }
 };
