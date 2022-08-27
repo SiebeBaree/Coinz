70d39e6b1295ad150ad9a8b8de7d6065339cc16c
Siebe Baree <siebe.baree@outlook.com>
2022-08-27 19:07:53 +0200

    Added airdrop support

--- src/models/Guild.js
+++ src/models/Guild.js
@@ -2,7 +2,9 @@ const { Schema, model } = require('mongoose');
 
 const Guild = Schema({
     id: { type: String, required: true, unique: true, index: true },
-    banned: { type: Boolean, default: false }
+    airdropStatus: { type: Boolean, default: false },
+    airdropChannel: { type: String, default: "" },
+    airdropNext: { type: Number, default: 0 }
 });
 
 module.exports = model('Guild', Guild, 'guilds');
