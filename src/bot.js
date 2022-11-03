dbf8f0ab16ce8fa4e99b3b3b91b9615de2c1cee5
Siebe Baree <siebe.baree@outlook.com>
2022-11-03 13:41:00 +0100

    Updated bot.js file

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,25 @@
 require('dotenv').config();
-const { ActivityType, GatewayIntentBits } = require('discord.js');
-const { schedule } = require('node-cron');
-const axios = require('axios');
-const { AutoPoster } = require('topgg-autoposter');
-const { connect } = require('mongoose');
+const { ActivityType, GatewayIntentBits, Partials } = require('discord.js');
+const { connect } = require("mongoose");
 
 const Bot = require('./structures/Bot.js');
-const bot = global.bot = new Bot({ intents: GatewayIntentBits.Guilds, presence: { activities: [{ name: "Only for Beta Testers", type: ActivityType.Watching }], status: "online" } });
+const bot = global.bot = new Bot({
+    partials: [Partials.Channel],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
+    presence: {
+        activities: [{
+            name: "Only for Beta Testers",
+            type: ActivityType.Watching
+        }], status: "online"
+    }
+});
 
+bot.login();
+bot.rest.on('rateLimit', rateLimitData => {
+    bot.logger.warn(`RATELIMITED for ${parseInt(rateLimitData.timeout / 1000)} seconds | Limit: ${rateLimitData.limit} requests | Global? ${rateLimitData.global ? "yes" : "no"}`);
+});
+
+// Connect to MongoDB Database
 connect(process.env.DATABASE_URI, {
     dbName: 'coinz_beta',
     useNewUrlParser: true,
@@ -19,35 +31,19 @@ connect(process.env.DATABASE_URI, {
     keepAliveInitialDelay: 300000
 }).then(() => bot.logger.ready('Connected to MongoDB'));
 
-bot.login();
-AutoPoster(process.env.API_TOPGG, bot);
+// Post stats to Top.gg & Discord Bot List
+require("./scripts/autoPoster.js");
 
-schedule('0 * * * *', async function () {
-    const promises = [
-        bot.shard.fetchClientValues('guilds.cache.size'),
-        bot.shard.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)),
-    ];
+// Create Webhook
+const app = require("./scripts/api.js");
 
-    Promise.all(promises)
-        .then(results => {
-            const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
-            const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+const port = process.env.PORT || 8700;
+app.listen(port, () => bot.logger.ready(`Vote Webhooks available on http://localhost:${port}`));
 
-            await axios.post('https://discordbotlist.com/api/v1/bots/938771676433362955/stats', {
-                guilds: totalGuilds,
-                users: totalMembers
-            }, {
-                headers: {
-                    "Authorization": process.env.API_DISCORDBOTLIST
-                }
-            });
-        }).catch();
-});
+// Load Crons
+require("./scripts/crons.js");
 
-bot.rest.on('rateLimit', rateLimitData => {
-    bot.logger.warn(`RATELIMITED for ${parseInt(rateLimitData.timeout / 1000)} seconds | Limit: ${rateLimitData.limit} requests | Global? ${rateLimitData.global ? "yes" : "no"}`);
-});
-
+// Global Error Handler
 const ignoredErrors = ["DiscordAPIError[10008]: Unknown Message"];
 process.on('uncaughtException', (err) => {
     if (!ignoredErrors.includes(`${err.name}: ${err.message}`)) {
