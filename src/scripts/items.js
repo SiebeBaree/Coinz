45fa2546743a733b4b3b4662b87263fb649190d4
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:50:40 +0100

    Updated items script

--- src/scripts/items.js
+++ src/scripts/items.js
@@ -1,59 +1,58 @@
-// What is this file?
-// Use this file to upload new items to your items collection
+import dotenv from "dotenv"
+dotenv.config()
 
-// How to use this file?
-// 1. Create a file 'src/data/shop/items.json'
-// 2. Copy the data from 'src/data/shop/template.json' and paste it in your file
-// 3. Add as many elements in the array you want.
-// EXTRA: elements with a empty string as itemId will be ignored
+import { connect } from "mongoose"
+import Item from "../models/Item.js"
+import itemData from "../assets/items.json" assert { type: "json" }
+import Logger from "../structures/Logger.js"
 
-// How to run this file?
-// npm run items
-
-const items = require('../assets/items.json').items;
-require('dotenv').config();
-const mongoose = require('mongoose');
-const itemsSchema = require('../models/Item');
-
-// If you enable this, you will override all existing items in the database
-const overrideItems = false;
+const logger = new Logger();
+const { items } = itemData;
+const OVERRIDE_ITEMS = false;
 
 async function createItem(item) {
-    let itemObj = await itemsSchema.findOne({ itemId: item.itemId });
+    let itemObj = await Item.findOne({ itemId: item.itemId });
     if (itemObj) {
-        if (!overrideItems) return true;
-        await itemsSchema.deleteOne({ itemId: item.itemId });
+        if (!OVERRIDE_ITEMS) return true;
+        await Item.deleteOne({ itemId: item.itemId });
     }
 
-    itemObj = new itemsSchema(item);
-    await itemObj.save().catch(err => console.log(err));
-    return itemObj && overrideItems ? true : false;
+    itemObj = new Item(item);
+    await itemObj.save().catch(err => logger.error(err));
+    return itemObj && OVERRIDE_ITEMS ? true : false;
 }
 
 (async () => {
-    // Connect to the database
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            dbName: 'coinz_beta',
+        const DB_NAME = process.env.NODE_ENV === "production" ? "coinz" : "coinz_beta";
+        connect(process.env.DATABASE_URI, {
+            dbName: DB_NAME,
+            useNewUrlParser: true,
+            maxPoolSize: 100,
+            minPoolSize: 5,
+            family: 4,
+            heartbeatFrequencyMS: 30000,
             keepAlive: true,
             keepAliveInitialDelay: 300000
-        });
-        console.log(`Connected to MongoDB.`);
+        }).then(() => logger.ready('Connected to MongoDB'));
     } catch (e) {
-        console.log('Unable to connect to MongoDB Database.\nError: ' + err);
+        logger.error('Unable to connect to MongoDB Database.\nError: ' + err);
+        process.exit(0);
     }
 
     for (let i = 0; i < items.length; i++) {
         if (items[i].itemId !== "") {
             let output = await createItem(items[i]);
 
-            if (output) {
-                console.log(`Item ${items[i].itemId} already exists.`);
+            if (output && !OVERRIDE_ITEMS) {
+                logger.log(`Item ${items[i].itemId} already exists.`);
             } else {
-                console.log(`Item ${items[i].itemId} was added to the database.`);
+                logger.log(`Item ${items[i].itemId} was added to the database.`);
             }
         } else {
-            console.log(`Item ${i} was empty...`);
+            logger.warn(`Item ${i} was empty...`);
         }
     }
-})();
+
+    process.exit(0);
+})()
