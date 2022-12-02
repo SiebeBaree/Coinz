c9c3abf9fcf323530c944ea3438f945377e62658
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:54:54 +0100

    Added option to buy spins with tickets

--- src/commands/economy/lucky-wheel.js
+++ src/commands/economy/lucky-wheel.js
@@ -1,9 +1,13 @@
-const Command = require('../../structures/Command.js');
-const { EmbedBuilder, ApplicationCommandOptionType } = require('discord.js');
-const items = require('../../assets/lootTables/luckyWheel.json').rewards;
-const MemberModel = require('../../models/Member');
+import Command from '../../structures/Command.js'
+import { EmbedBuilder, ApplicationCommandOptionType } from 'discord.js'
+import { timeout, getRandomLoot } from '../../lib/helpers.js'
+import { addItem } from '../../lib/user.js'
+import MemberModel from '../../models/Member.js'
+import Stats from '../../models/Stats.js'
+import luckyWheelJson from '../../assets/loot/luckyWheel.json' assert { type: "json" }
+const items = luckyWheelJson.rewards;
 
-class LuckyWheel extends Command {
+export default class extends Command {
     info = {
         name: "lucky-wheel",
         description: "Spin the lucky wheel and get awesome rewards",
@@ -25,7 +29,7 @@ class LuckyWheel extends Command {
                         description: 'How many spins you want to do in 1 command. | default: 1.',
                         required: false,
                         min_value: 1,
-                        max_value: 25
+                        max_value: 20
                     }
                 ]
             },
@@ -40,7 +44,7 @@ class LuckyWheel extends Command {
                         description: 'How many spins you want to buy. | default: 1.',
                         required: false,
                         min_value: 1,
-                        max_value: 25
+                        max_value: 20
                     }
                 ]
             }
@@ -53,17 +57,20 @@ class LuckyWheel extends Command {
         deferReply: false
     };
 
+    ticketPrice = 10;
+
     constructor(...args) {
         super(...args);
     }
 
     async run(interaction, data) {
-        if (interaction.options.getSubcommand() === "rewards") return await this.execRewards(interaction, data);
+        if (interaction.options.getSubcommand() === "info") return await this.execInfo(interaction, data);
         if (interaction.options.getSubcommand() === "spin") return await this.execSpin(interaction, data);
-        return await interaction.reply({ content: `Sorry, invalid arguments. Please try again.\nIf you don't know how to use this command use \`/help ${this.info.name}\`.`, ephemeral: true });
+        if (interaction.options.getSubcommand() === "buy") return await this.execBuy(interaction, data);
+        return await interaction.reply({ content: `Sorry, invalid arguments. Please try again.\nIf you don't know how to use this command use \`/help command ${this.info.name}\`.`, ephemeral: true });
     }
 
-    async execRewards(interaction, data) {
+    async execInfo(interaction, data) {
         await interaction.deferReply();
         const spinsLeft = data.user.spins === undefined ? 0 : data.user.spins;
 
@@ -73,6 +80,8 @@ class LuckyWheel extends Command {
 
             if (parsedItem[0] === "money") {
                 rewardsTxt += `Money: :coin: ${parsedItem[1]}\n`;
+            } else if (parsedItem[0] === "tickets") {
+                rewardsTxt += `**${parsedItem[1]}x** Tickets <:ticket:1032669959161122976> \n`;
             } else {
                 const item = await bot.database.fetchItem(parsedItem[0]);
                 rewardsTxt += `**${parsedItem[1]}x** ${item.name} <:${item.itemId}:${item.emoteId}>\n`;
@@ -82,10 +91,10 @@ class LuckyWheel extends Command {
         const embed = new EmbedBuilder()
             .setTitle(`Lucky Wheel Rewards`)
             .setColor(bot.config.embed.color)
-            .setDescription(`:gift: **To spin the wheel, please vote for Coinz using the** </vote:993095062726647810> **command.**\n:warning: **You get 1 free spin per vote!**\n:star: **You have ${spinsLeft}x ${spinsLeft === 1 ? "spin" : "spins"} left.**`)
+            .setDescription(`:gift: **</lucky-wheel buy:1005435550884442193> or </vote:993095062726647810> to get more spins!**\n:moneybag: **Wheel Spins cost <:ticket:1032669959161122976> ${this.ticketPrice} per spin.**\n:warning: **You get 1 free spin per vote and double spins in the weekend.**\n:gem: **Premium users always get double spins, more info [here](https://coinzbot.xyz/store).**\n:star: **You have ${spinsLeft}x ${spinsLeft === 1 ? "spin" : "spins"} left**`)
             .setFooter({ text: bot.config.embed.footer })
             .addFields(
-                { name: `Rewards`, value: `${rewardsTxt}`, inline: false }
+                { name: `Possible Rewards`, value: `${rewardsTxt}`, inline: false }
             )
 
         await interaction.editReply({ embeds: [embed] });
@@ -95,7 +104,7 @@ class LuckyWheel extends Command {
         const amount = interaction.options.getInteger('amount') || 1;
 
         if (data.user.spins === undefined || data.user.spins <= 0) {
-            return await interaction.reply({ content: `You don't have any spins left. If you want to spin the lucky wheel, consider voting (</vote:993095062726647810>). You get 1x free spin for each vote.`, ephemeral: true });
+            return await interaction.reply({ content: `You don't have any spins left. If you want to spin the lucky wheel, consider voting (</vote:993095062726647810>). You get 1x free spin for each vote and double spins in the weekend.`, ephemeral: true });
         } else if (data.user.spins < amount) {
             return await interaction.reply({ content: `You only have ${data.user.spins} spins left.`, ephemeral: true });
         }
@@ -109,36 +118,53 @@ class LuckyWheel extends Command {
             .setImage(`https://cdn.coinzbot.xyz/games/extra/luckywheel.gif`)
 
         await interaction.editReply({ embeds: [preEmbed] });
-        await bot.tools.timeout(3000);
+        await timeout(3000);
 
-        const loot = bot.tools.getRandomLoot(items, amount || 1);
+        const loot = getRandomLoot(items, amount || 1);
         let mappedLoot = loot.reduce((cnt, cur) => (cnt[cur] = cnt[cur] + 1 || 1, cnt), {});
         let keys = Object.keys(mappedLoot);
 
         let rewardsTxt = "";
         let totalPrice = 0;
-        let bonus = 0;
+        let money = 0;
+        let tickets = 0;
 
         for (let i = 0; i < keys.length; i++) {
             const parsedItem = keys[i].split('-');
 
             if (parsedItem[0] === "money") {
-                bonus += parseInt(parsedItem[1]);
+                money += parseInt(parsedItem[1]);
+            } else if (parsedItem[0] === "tickets") {
+                tickets += parseInt(parsedItem[1]);
             } else {
                 const item = await bot.database.fetchItem(parsedItem[0]);
                 totalPrice += item.sellPrice * parseInt(parsedItem[1]);
                 rewardsTxt += `${mappedLoot[keys[i]] * parseInt(parsedItem[1])}x ${item.name} <:${item.itemId}:${item.emoteId}>\n`;
-                await bot.tools.addItem(interaction.member.id, item.itemId, mappedLoot[keys[i]] * parseInt(parsedItem[1]), data.user.inventory);
+                await addItem(interaction.member.id, item.itemId, mappedLoot[keys[i]] * parseInt(parsedItem[1]), data.user.inventory);
             }
         }
 
-        if (rewardsTxt === "") rewardsTxt = "You got no extra loot. You only got money.";
-        let bonusTxt = bonus === 0 ? "" : `\n:moneybag: **You also got a :coin: ${bonus} bonus!**`;
+        if (rewardsTxt === "") rewardsTxt = "You got no extra loot. You only got money and/or tickets.";
+        let bonusTxt = money === 0 && tickets === 0 ? "" : `\n:moneybag: **You also got :coin: ${money} and <:ticket:1032669959161122976> ${tickets} extra!**`;
 
         await MemberModel.updateOne(
             { id: interaction.member.id },
-            { $inc: { spins: -amount, wallet: bonus } }
+            { $inc: { spins: -amount, wallet: money, tickets: tickets } }
         );
+
+        await Stats.updateOne(
+            { id: interaction.member.id },
+            { $inc: { luckyWheelSpins: amount } },
+            { upsert: true }
+        );
+
+        const userStats = await Stats.findOne({ id: interaction.member.id });
+        if (userStats.luckyWheelSpins >= 50 && !data.user.badges.includes("feeling_lucky")) {
+            await MemberModel.updateOne(
+                { id: interaction.member.id },
+                { $push: { badges: "feeling_lucky" } }
+            );
+        }
 
         const embed = new EmbedBuilder()
             .setAuthor({ name: `${interaction.member.displayName || interaction.member.username}'s Lucky Wheel`, iconURL: `${interaction.member.displayAvatarURL() || bot.config.embed.defaultIcon}` })
@@ -151,6 +177,20 @@ class LuckyWheel extends Command {
 
         await interaction.editReply({ embeds: [embed] });
     }
-}
 
-module.exports = LuckyWheel;
+    async execBuy(interaction, data) {
+        const amount = interaction.options.getInteger('amount') || 1;
+
+        if (data.user.tickets < this.ticketPrice * amount) {
+            return await interaction.reply({ content: `You don't have enough tickets. You need <:ticket:1032669959161122976> ${this.ticketPrice * amount} to spin the wheel ${amount} ${amount === 1 ? "time" : "times"}.`, ephemeral: true });
+        }
+
+        await interaction.deferReply({ ephemeral: true });
+        await MemberModel.updateOne(
+            { id: interaction.member.id },
+            { $inc: { spins: amount, tickets: -this.ticketPrice * amount } }
+        );
+
+        await interaction.editReply({ content: `You bought ${amount} ${amount === 1 ? "spin" : "spins"} for <:ticket:1032669959161122976> ${this.ticketPrice * amount}.`, ephemeral: true });
+    }
+}
