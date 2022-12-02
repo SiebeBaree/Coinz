4622bcc5b1b9926ecd1deba78ca2a82ae2728795
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:55:24 +0100

    Fixed list subcommand

--- src/commands/economy/lootbox.js
+++ src/commands/economy/lootbox.js
@@ -1,8 +1,9 @@
-const Command = require('../../structures/Command.js');
-const { EmbedBuilder, ApplicationCommandOptionType } = require('discord.js');
-const lootboxes = require("../../assets/lootboxes.json");
+import Command from '../../structures/Command.js'
+import { EmbedBuilder, ApplicationCommandOptionType } from 'discord.js'
+import { randomNumber } from '../../lib/helpers.js'
+import lootboxes from '../../assets/lootboxes.json' assert { type: 'json' }
 
-class Lootbox extends Command {
+export default class extends Command {
     info = {
         name: "lootbox",
         description: "Open or buy a lootbox to get awesome rewards.",
@@ -32,28 +33,7 @@ class Lootbox extends Command {
                         required: true
                     }
                 ]
-            },
-            {
-                name: 'buy',
-                type: ApplicationCommandOptionType.Subcommand,
-                description: 'Buy a new lootbox.',
-                options: [
-                    {
-                        name: 'lootbox-id',
-                        type: ApplicationCommandOptionType.String,
-                        description: 'The name of the lootbox you want to buy.',
-                        required: true
-                    },
-                    {
-                        name: 'amount',
-                        type: ApplicationCommandOptionType.Integer,
-                        description: 'The amount of lootboxes you want to buy. | default: 1.',
-                        required: false,
-                        min_value: 1,
-                        max_value: 10
-                    }
-                ]
-            },
+            }
         ],
         category: "economy",
         extraFields: [],
@@ -70,8 +50,7 @@ class Lootbox extends Command {
     async run(interaction, data) {
         if (interaction.options.getSubcommand() === "list") return await this.execList(interaction, data);
         if (interaction.options.getSubcommand() === "open") return await this.execOpen(interaction, data);
-        if (interaction.options.getSubcommand() === "buy") return await this.execBuy(interaction, data);
-        return await interaction.reply({ content: `Sorry, invalid arguments. Please try again.\nIf you don't know how to use this command use \`/help ${this.info.name}\`.`, ephemeral: true });
+        return await interaction.reply({ content: `Sorry, invalid arguments. Please try again.\nIf you don't know how to use this command use \`/help command ${this.info.name}\`.`, ephemeral: true });
     }
 
     async execList(interaction, data) {
@@ -81,39 +60,43 @@ class Lootbox extends Command {
             await interaction.deferReply();
             let lootboxStr = "";
 
-            for (const lootboxIndex in this.lootboxIds) {
-                const lootbox = this.lootboxIds[lootboxIndex];
-                let icon = `<:${lootbox}:${lootboxes[lootbox].emoteId}>`;
-                lootboxStr += `${icon} **${lootbox}** ― :tickets: ${lootboxes[lootbox].price}\n> ${lootboxes[lootbox].description}\n\n`;
+            const lootboxKeys = Object.keys(lootboxes);
+            for (let i = 0; i < lootboxKeys.length; i++) {
+                const lootbox = lootboxes[lootboxKeys[i]];
+                let icon = `<:${lootboxKeys[i]}:${lootbox.emoteId}>`;
+                lootboxStr += `${icon} **${lootbox.name}** (ID: \`${lootboxKeys[i]}\`) ― <:ticket:1032669959161122976> ${lootbox.price}\n\n`;
             }
 
             if (lootboxStr !== "") lootboxStr = "\n\n" + lootboxStr;
             const embed = new EmbedBuilder()
                 .setTitle('List of Lootboxes')
                 .setColor(bot.config.embed.color)
-                .setDescription(`Lootboxes are bought using tickets. Use </premium:0> to get more information..${lootboxStr}`)
+                .setDescription(`:toolbox: **Lootboxes are bought using tickets.**\n:gem: **Use** \`/premium\` **to get more information.**${lootboxStr}`)
                 .setFooter({ text: "Get more info using /lootbox list <lootbox-id>" });
 
             return await interaction.editReply({ embeds: [embed] });
         } else {
             const lootbox = lootboxes[name.toLowerCase()];
-            if (!lootbox) return await interaction.reply({ content: 'This lootbox doesn\'t exist! Use </lootbox list:0> to get a list with all lootboxes.', ephemeral: true });
+            if (!lootbox) return await interaction.reply({ content: 'This lootbox doesn\'t exist! Use \`/lootbox list\` to get a list with all lootboxes.', ephemeral: true });
 
             await interaction.deferReply();
-            const promises = lootbox.loot.map(async ({ itemId, chance }) => {
-                const item = await bot.database.fetchItem(itemId);
-                return `<:${itemId}:${item.emoteId}> **${item.name}** - ${chance}% chance`;
+            const promises = lootbox.loot.map(async (lootItem) => {
+                if (["Coins", "Tickets"].includes(lootItem.itemId)) {
+                    return `<:${lootItem.emoteName}:${lootItem.emoteId}> **${lootItem.amount}** ${lootItem.itemId}`;
+                } else {
+                    const item = await bot.database.fetchItem(lootItem.itemId);
+                    return `<:${lootItem.itemId}:${item.emoteId}> **${lootItem.amount}x** ${item.name}`;
+                }
             });
 
             Promise.all(promises)
                 .then(results => {
                     const embed = new EmbedBuilder()
-                        .setTitle(`Lootbox: ${name.toLowerCase()}`)
+                        .setTitle(lootbox.name)
                         .setColor(bot.config.embed.color)
                         .setThumbnail(`https://cdn.discordapp.com/emojis/${lootbox.emoteId}.${lootbox.animated ? "gif" : "png"}`)
-                        .setDescription(`> ${lootbox.description}`)
                         .addFields(
-                            { name: 'Price', value: `:tickets: ${lootbox.price}`, inline: false },
+                            { name: 'Price', value: `<:ticket:1032669959161122976> ${lootbox.price}`, inline: false },
                             { name: 'Possible Loot', value: results.join("\n"), inline: false }
                         )
 
@@ -123,27 +106,14 @@ class Lootbox extends Command {
     }
 
     async execOpen(interaction, data) {
-        const name = interaction.options.getString('lootbox-id');
-        const lootbox = lootboxes[name.toLowerCase()];
-        if (!lootbox) return await interaction.reply({ content: 'This lootbox doesn\'t exist! Use </lootbox list:0> to get a list with all lootboxes.', ephemeral: true });
-
-        if (data.user.lootboxes === undefined) data.user.lootboxes = [];
-        const lootboxUser = data.user.lootboxes.find(l => l.itemId === name.toLowerCase());
-        if (lootboxUser === null) return await interaction.reply({ content: `You don't own this lootbox. Please buy this lootbox using \`/lootbox buy ${name.toLowerCase()} [amount]\``, ephemeral: true });
-
-        await interaction.deferReply();
-        const item = await getRandomItem(lootbox.loot);
-    }
-
-    async execBuy(interaction, data) {
-
+        await interaction.reply({ content: 'You can\'t open lootboxes at the moment. You can still collect them and open them later...', ephemeral: true });
     }
 
     async getRandomItem(loot) {
         let item;
 
         for (let i = 0; i < loot.length; i++) {
-            const random = bot.tools.randomNumber(1, 1000);
+            const random = randomNumber(1, 1000);
             if (random <= loot[i].chance) {
                 item = loot[i];
                 break;
@@ -158,5 +128,3 @@ class Lootbox extends Command {
         };
     }
 }
-
-module.exports = Lootbox;
