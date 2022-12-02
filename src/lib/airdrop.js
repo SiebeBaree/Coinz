28558aa8b3c9a36d5c98bc8425d98295c50f303e
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:53:04 +0100

    Bug fixes

--- src/lib/airdrop.js
+++ src/lib/airdrop.js
@@ -1,19 +1,19 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors, ComponentType, PermissionsBitField } = require("discord.js");
-const database = require("./database");
-const GuildModel = require("../models/Guild");
-const MemberModel = require("../models/Member");
-const rewards = require("../assets/lootTables/rewards.json");
+import { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors, ComponentType, PermissionsBitField } from "discord.js"
+import { fetchItem, fetchMember } from "./database.js"
+import Guild from "../models/Guild.js"
+import Member from "../models/Member.js"
+import rewards from "../assets/loot/airdrop.json" assert { type: "json" }
 const keys = Object.keys(rewards);
 
-const processAirdrop = async (client, guildData) => {
-    const guild = await client.guilds.fetch(guildData.id);
+export const processAirdrop = async (guildData) => {
+    const guild = await bot.guilds.fetch(guildData.id);
     const channel = await guild.channels.fetch(guildData.airdropChannel);
 
     if (!guild.members.me.permissions.has(PermissionsBitField.Flags.SendMessages) || !guild.members.me.permissionsIn(channel).has(PermissionsBitField.Flags.SendMessages)) {
         return await resetAirdrop(guildData.id);
     }
 
-    await GuildModel.updateOne(
+    await Guild.updateOne(
         { id: guildData.id },
         { $set: { airdropNext: parseInt(Date.now() / 1000) + randomNumber(3600, 7200) } },
         { upsert: true }
@@ -30,7 +30,7 @@ const processAirdrop = async (client, guildData) => {
             totalMoney += parseInt(rewards[mappedKeys[i]] * mappedLoot[mappedKeys[i]]);
             rewardsText += `Money: :coin: ${rewards[mappedKeys[i]] * mappedLoot[mappedKeys[i]]}\n`;
         } else {
-            const item = await database.fetchItem(mappedKeys[i]);
+            const item = await fetchItem(mappedKeys[i]);
             rewardsText += `${rewards[mappedKeys[i]] * mappedLoot[mappedKeys[i]]}x **${item.name}** <:${item.itemId}:${item.emoteId}>\n`;
         }
     }
@@ -59,7 +59,7 @@ const processAirdrop = async (client, guildData) => {
             buttonIsPressed = true;
             const userId = i.member.id;
 
-            let memberData = await database.fetchMember(userId);
+            let memberData = await fetchMember(userId);
             if (memberData.lastAirdrop === undefined) memberData.lastAirdrop = 0;
             if (parseInt(Date.now() / 1000) - memberData.lastAirdrop >= 3600) {
                 const newEmbed = new EmbedBuilder()
@@ -69,19 +69,19 @@ const processAirdrop = async (client, guildData) => {
                     .addFields({ name: "Rewards", value: rewardsText, inline: false })
                 await message.edit({ embeds: [newEmbed], components: [getRow(true)] });
 
-                await MemberModel.updateOne({ id: userId }, { $set: { lastAirdrop: parseInt(Date.now() / 1000) } });
+                await Member.updateOne({ id: userId }, { $set: { lastAirdrop: parseInt(Date.now() / 1000) } });
+                if (totalMoney > 0) await Member.updateOne({ id: userId }, { $inc: { wallet: totalMoney } });
 
-                if (totalMoney > 0) await MemberModel.updateOne({ id: userId }, { $inc: { wallet: totalMoney } });
                 for (let i = 0; i < mappedKeys.length; i++) {
                     if (mappedKeys[i].startsWith("money")) continue;
-                    const userData = await database.fetchMember(userId);
+                    const userData = await fetchMember(userId);
                     await addItem(userId, mappedKeys[i], rewards[mappedKeys[i]] * mappedLoot[mappedKeys[i]], userData.inventory);
                 }
             } else {
                 buttonIsPressed = false;
 
                 try {
-                    const member = await client.users.fetch(userId);
+                    const member = await bot.users.fetch(userId);
                     const dmChannel = await member.createDM();
                     await dmChannel.send({ content: `You already collected an airdrop in the last 60 minutes.` });
                     await member.deleteDM();
@@ -107,26 +107,40 @@ const processAirdrop = async (client, guildData) => {
 };
 
 const resetAirdrop = async (guildId) => {
-    await GuildModel.updateOne(
-        { id: guildId },
-        {
-            $set: {
-                airdropNext: 0,
-                airdropChannel: "",
-                airdropStatus: false
-            }
-        },
-        { upsert: true }
-    );
+    const guild = await Guild.findOne({ id: guildId });
+
+    if (guild.airdropTries > 5) {
+        await Guild.updateOne(
+            { id: guildId },
+            {
+                $set: {
+                    airdropNext: 0,
+                    airdropChannel: "",
+                    airdropStatus: false,
+                    airdropTries: 0
+                }
+            },
+            { upsert: true }
+        );
+    } else {
+        await Guild.updateOne(
+            { id: guildId },
+            {
+                $set: { airdropNext: parseInt(Date.now() / 1000) + randomNumber(1800, 3600) },
+                $inc: { airdropTries: 1 }
+            },
+            { upsert: true }
+        );
+    }
 };
 
 const addItem = async (userId, itemId, quantity = 1, inventory = []) => {
     if (checkItem(inventory, itemId)) {
-        await MemberModel.updateOne({ id: userId, 'inventory.itemId': itemId }, {
+        await Member.updateOne({ id: userId, 'inventory.itemId': itemId }, {
             $inc: { 'inventory.$.quantity': quantity }
         });
     } else {
-        await MemberModel.updateOne({ id: userId }, {
+        await Member.updateOne({ id: userId }, {
             $push: {
                 inventory:
                 {
@@ -175,6 +189,6 @@ const getRow = (disable = false) => {
     return new ActionRowBuilder().addComponents(button);
 };
 
-module.exports = {
+export default {
     processAirdrop
-};
+}
