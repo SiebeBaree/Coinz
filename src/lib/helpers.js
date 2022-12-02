3ad7ebf77d757271e0da8f2e9ffb85a890ab00d2
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:51:37 +0100

    Created from utils.js

---
+++ src/lib/helpers.js
@@ -0,0 +1,77 @@
+export const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export const commandPassed = (chance) => {
+    return chance >= randomNumber(1, 100);
+}
+
+export const timeout = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const msToTime = (ms) => {
+    const seconds = Math.floor((ms / 1000) % 60);
+    const minutes = Math.floor((ms / (1000 * 60)) % 60);
+    const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+    const days = Math.floor((ms / (1000 * 60 * 60 * 24)));
+
+    let str = "";
+    str += days > 0 ? days + "d " : "";
+    str += hours > 0 ? hours + "h " : "";
+    str += minutes > 0 ? minutes + "m " : "";
+    str += seconds > 0 ? seconds + "s" : "";
+
+    return str || "0s";
+}
+
+export const roundNumber = (n, places = 2) => {
+    const x = Math.pow(10, places);
+    return Math.round(n * x) / x;
+}
+
+export const getRandomLoot = (lootTable, min, max = 0) => {
+    const quantity = max === 0 || max <= min ? min : randomNumber(min, max);
+
+    let loot = [];
+    for (let i = 0; i < quantity; i++) loot.push(lootTable[randomNumber(0, lootTable.length - 1)]);
+    return loot === [] ? [lootTable[0]] : loot;
+}
+
+export const extractNumber = (number) => {
+    if (/^[0-9]+$/.test(number)) return parseInt(number);
+
+    let multiplier = number.substr(-1).toLowerCase();
+    if (multiplier == "k") {
+        return parseInt(parseFloat(number) * 1000);
+    } else if (multiplier == "m") {
+        return parseInt(parseFloat(number) * 1000000);
+    }
+}
+
+export const checkBet = (betStr, user, minBet = 50, maxBet = 5000) => {
+    let bet = 0;
+    if (["all", "max"].includes(betStr.toLowerCase())) {
+        if (user.wallet <= 0) return `You don't have any money in your wallet.`;
+        bet = user.wallet > maxBet ? maxBet : user.wallet;
+    } else {
+        bet = extractNumber(betStr);
+        if (bet === undefined) return "That's not a correct bet. Please use numbers or `1k` for example.";
+        if (bet < minBet) return `The minimum bet is :coin: ${minBet}.`;
+        if (bet > maxBet) return `You can only bet a maximum of :coin: ${maxBet}.`;
+        if (bet > user.wallet) return `You don't have :coin: ${bet} in your wallet.`;
+    }
+
+    return bet;
+}
+
+export default {
+    randomNumber,
+    commandPassed,
+    timeout,
+    msToTime,
+    roundNumber,
+    getRandomLoot,
+    extractNumber,
+    checkBet
+}
---
+++ src/lib/user.js
@@ -0,0 +1,106 @@
+import { fetchMember } from './database.js'
+import MemberModel from '../models/Member.js'
+import { createLog } from './database.js'
+import Business from '../models/Business.js'
+
+export const addMoney = async (userId, amount) => {
+    if (Math.abs(amount) > 1000) await createLog(`${amount >= 0 ? "Adding" : "Removing"} ${amount} to ${userId}`, 'addMoney', userId);
+    await MemberModel.updateOne({ id: userId }, { $inc: { wallet: amount } });
+}
+
+export const takeMoney = async (userId, amount, goNegative = true) => {
+    const UserData = await fetchMember(userId);
+    if (UserData.wallet - amount < 0 && !goNegative) amount = UserData.wallet;
+    await addMoney(userId, -amount);
+}
+
+export const addItem = async (userId, itemId, quantity = 1, inventory = []) => {
+    if (checkItem(inventory, itemId)) {
+        await MemberModel.updateOne({ id: userId, 'inventory.itemId': itemId }, {
+            $inc: { 'inventory.$.quantity': quantity }
+        });
+    } else {
+        await MemberModel.updateOne({ id: userId }, {
+            $push: {
+                inventory:
+                {
+                    itemId: itemId,
+                    quantity: quantity
+                }
+            },
+        });
+    }
+}
+
+export const takeItem = async (userId, itemId, inventory, quantity = 1) => {
+    const item = checkItem(inventory, itemId, true);
+    if (!item) return false;
+
+    if (item.quantity - quantity > 0) {
+        await MemberModel.updateOne({ id: userId, 'inventory.itemId': item.itemId }, {
+            $inc: { 'inventory.$.quantity': -quantity }
+        });
+    } else {
+        await MemberModel.updateOne({ id: userId }, {
+            $pull: { 'inventory': { itemId: item.itemId } }
+        });
+    }
+
+    return true;
+}
+
+export const checkItem = (inventory, itemId, new_ = false) => {
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i].itemId === itemId) {
+            return new_ === true ? inventory[i] : true;
+        }
+    }
+
+    return false;
+}
+
+export const getBusiness = async (userData) => {
+    let returnData = {
+        userId: userData.id,
+        company: null,
+        isOwner: false,
+        employee: null,
+    }
+
+    if (userData.job === 'business') {
+        const company = await Business.findOne({ ownerId: userData.id });
+        if (company) {
+            returnData.company = company;
+            returnData.isOwner = true;
+        }
+    } else if (userData.job.startsWith('business-')) {
+        const ownerId = userData.job.split('-')[1];
+        const company = await Business.findOne({ ownerId: ownerId });
+
+        if (company) {
+            let isEmployee = false;
+            for (let i = 0; i < company.employees.length; i++) {
+                if (company.employees[i].userId === userData.id) {
+                    isEmployee = true;
+                    returnData.employee = company.employees[i];
+                    break;
+                }
+            }
+
+            if (isEmployee) {
+                returnData.company = company;
+            }
+        }
+    }
+
+    return returnData;
+}
+
+export default {
+    addMoney,
+    takeMoney,
+    addItem,
+    takeItem,
+    checkItem,
+    getBusiness
+}
