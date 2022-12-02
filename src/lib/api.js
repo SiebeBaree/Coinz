900fa3b0d11b2c9dfd8faed9214c87a48d4588eb
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:52:39 +0100

    Moved vote processing to another file

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,14 +1,35 @@
-require("dotenv").config();
-const { Webhook } = require("@top-gg/sdk");
-const express = require("express");
-const { processVote } = require("../scripts/vote.js");
-
-const topggWebhook = new Webhook(process.env.TOPGG_VOTE_WEBHOOK);
+import { Webhook } from "@top-gg/sdk"
+import express from "express"
+import { processVote } from "./vote.js"
+import { schedule } from "node-cron"
+import axios from "axios"
+import { AutoPoster } from "topgg-autoposter"
+import { readFile } from 'fs/promises'
 
 const app = express();
-app.use(express.json());
 
-app.post("/topggwebhook", topggWebhook.listener(async (vote) => { await processVote(vote.user, "top.gg"); }));
-app.post("/dblwebhook", async (request) => { await processVote(request.body.id, "dbl") });
+if (process.env.NODE_ENV === "production") {
+    app.use(express.json());
 
-module.exports = app;
+    const topggWebhook = new Webhook(process.env.TOPGG_VOTE_WEBHOOK);
+    app.post("/topggwebhook", topggWebhook.listener(async (vote) => { await processVote(vote.user, "top.gg"); }));
+    app.post("/dblwebhook", async (request) => { await processVote(request.body.id, "dbl") });
+
+    AutoPoster(process.env.API_TOPGG, bot);
+    schedule('0 * * * *', async function () {
+        const stats = JSON.parse(
+            await readFile(new URL('./src/assets/stats.json', import.meta.url))
+        );
+
+        await axios.post('https://discordbotlist.com/api/v1/bots/938771676433362955/stats', {
+            guilds: stats.guilds,
+            users: stats.members
+        }, {
+            headers: {
+                "Authorization": process.env.API_DISCORDBOTLIST
+            }
+        });
+    });
+}
+
+export default app;
