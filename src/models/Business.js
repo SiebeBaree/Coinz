9023085ae3bce0dbdbb8057c1242cbcce0071b53
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:50:19 +0100

    Updated business model

--- src/models/Business.js
+++ src/models/Business.js
@@ -1,33 +1,37 @@
-const { Schema, model } = require('mongoose');
-
-const Factory = Schema({
-    factoryId: { type: Number, required: true },
-    level: { type: Number, default: 1 },
-    product: { type: String, default: "" },
-    status: { type: String, default: "standby" },
-    collectOn: { type: Number, required: true, default: parseInt(Date.now() / 1000) },
-    maintenanceLevel: { type: Number, min: 0, max: 100, default: 100 }
-});
+import { Schema, model } from 'mongoose';
 
 const Employee = Schema({
     userId: { type: String, required: true },
-    role: { type: String, required: true },
-    wage: { type: Number, default: 15 }
+    role: { type: String, default: 'employee' },
+    wage: { type: Number, default: 15 },
+    hiredAt: { type: Date, default: Date.now() },
+    moneyCollected: { type: Number, default: 0 },
+    timesWorked: { type: Number, default: 0 }
 });
 
 const Inventory = Schema({
     itemId: { type: String, required: true },
-    quantity: { type: Number, default: 1 }
+    amount: { type: Number, default: 1 }
 });
 
-const Company = Schema({
-    id: { type: String, required: true, unique: true, index: true },
-    name: { type: String, default: "An Unnamed Company" },
-    balance: { type: Number, default: 0 },
-    taxRate: { type: Number, min: 0, max: 100, default: 10 },
+const Factory = Schema({
+    factoryId: { type: Number, required: true },
+    level: { type: Number, default: 1 },
+    experience: { type: Number, default: 0 },
+    production: { type: String, default: "none" },
+    status: { type: String, default: "standby" },
+    produceOn: { type: Date, default: Date.now() }
+});
+
+const Business = Schema({
+    ownerId: { type: String, required: true, unique: true, index: true },
+    name: { type: String, required: true },
+    balance: { type: Number, default: 500 },
+    taxRate: { type: Number, min: 0, max: 100, default: 5 },
+    risk: { type: Number, min: 0, max: 100, default: 0 },
     employees: [{ type: Employee }],
-    factories: [{ type: Factory }],
-    inventory: [{ type: Inventory }]
+    inventory: [{ type: Inventory }],
+    factories: [{ type: Factory }]
 });
 
-module.exports = model('Company', Company, 'companies');
+export default model('Business', Business, 'businesses');
