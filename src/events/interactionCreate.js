9f822b710d7e145fa06f4792945c8ec167d1c968
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:53:13 +0100

    Added premium support

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,7 +1,15 @@
-const Event = require('../structures/Event.js');
-const StatsModel = require('../models/Stats');
+import Event from "../structures/Event.js"
+import { EmbedBuilder } from 'discord.js'
+import Stats from "../models/Stats.js"
+import { msToTime } from "../lib/helpers.js"
 
-module.exports = class extends Event {
+const embed = new EmbedBuilder()
+    .setColor(bot.config.embed.color)
+    .setDescription("This command is only for **premium users**.\nIf you want to use this command, consider buying **Coinz Premium**.\nGo to the [**store**](https://coinzbot.xyz/store) to learn more.")
+    .setTimestamp()
+    .setFooter({ text: bot.config.embed.footer })
+
+export default class extends Event {
     constructor(...args) {
         super(...args);
     }
@@ -10,31 +18,28 @@ module.exports = class extends Event {
         if (!interaction.guild) return; // Return if dms or group chat
         if (interaction.member.bot) return; // Return if member is bot
 
-        //Checking if the message is a command
         const cmd = this.commands.get(interaction.commandName);
-
-        //If it isn't a command then return
         if (!cmd) return;
 
-        // const ban = await this.database.fetchBan(interaction.member.id);
-        // if (ban.guild.isBanned) {
-        //     return await interaction.reply({ content: `This server is banned from using Coinz.\n**Reason:** ${ban.guild.reason}\n\nIf you are the owner of the server, please join our [**support server**](https://discord.gg/asnZQwc6kW) if you think this ban was a mistake.`, ephemeral: true });
-        // } else if (ban.user.isBanned) {
-        //     return await interaction.reply({ content: `You are banned from using Coinz.\n**Reason:** ${ban.user.reason}\n\nPlease join our [**support server**](https://discord.gg/asnZQwc6kW) if you think this ban was a mistake.`, ephemeral: true });
-        // }
+        const premium = await this.database.fetchPremium(interaction.member.id, false);
+        if (cmd.info.isPremium === true) {
+            embed.setAuthor({ name: "Coinz Premium Required", iconURL: `${interaction.member.displayAvatarURL() || bot.config.embed.defaultIcon}` });
+            if (!premium.premium) return await interaction.reply({ embeds: [embed] });
+        }
 
-        if (await cmd.cool(cmd.info.name, interaction.member, cmd.info.cooldown)) {
-            return await interaction.reply({ content: `:x: You have to wait ${this.tools.msToTime(await this.cooldown.getCooldown(interaction.member.id, cmd.info.name) * 1000)} to use this command again.`, ephemeral: true });
+        const cooldown = cmd.info.cooldown;
+        if (await cmd.cool(cmd.info.name, interaction.member, cooldown === 0 && premium.premium ? 1 : cooldown)) {
+            return await interaction.reply({ content: `:x: You have to wait ${msToTime(await this.cooldown.getCooldown(interaction.member.id, cmd.info.name) * 1000)} to use this command again.`, ephemeral: true });
         }
 
         if (cmd.info.deferReply === true) await interaction.deferReply();
         const memberData = cmd.info.memberRequired === true ? await this.database.fetchMember(interaction.member.id) : undefined;
-        await cmd.run(interaction, { user: memberData });
+        await cmd.run(interaction, { user: memberData, premium: premium });
 
-        await StatsModel.updateOne(
+        await Stats.updateOne(
             { id: interaction.member.id },
             { $inc: { commandsExecuted: 1 } },
             { upsert: true }
         );
     }
-};
+}
