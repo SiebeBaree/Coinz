404ab1ec0edbf020ca09831ccde887c69eefd9f4
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:51:02 +0100

    Added new statistic

--- src/models/Stats.js
+++ src/models/Stats.js
@@ -1,11 +1,12 @@
-const { Schema, model } = require('mongoose');
+import { Schema, model } from 'mongoose';
 
 const Stats = Schema({
     id: { type: String, required: true, unique: true, index: true },
     workComplete: { type: Number, default: 0 },
     commandsExecuted: { type: Number, default: 0 },
     catchedFish: { type: Number, default: 0 },
-    diamondsFound: { type: Number, default: 0 }
+    diamondsFound: { type: Number, default: 0 },
+    luckyWheelSpins: { type: Number, default: 0 }
 });
 
-module.exports = model('Stats', Stats, 'stats');
+export default model('Stats', Stats, 'stats');
