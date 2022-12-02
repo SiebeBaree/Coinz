ceb81dc81a6b10f1a8313a9991d35e7082290d3c
Siebe Baree <siebe.baree@outlook.com>
2022-12-02 19:57:30 +0100

    Bug fixes

--- src/commands/economy/work.js
+++ src/commands/economy/work.js
@@ -1,19 +1,25 @@
-const Command = require('../../structures/Command.js');
-const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
-const workList = require('../../assets/jobList.json').jobs;
-const wordList = require('../../assets/wordlist.json').words;
-const MemberModel = require('../../models/Member');
-const CompanyModel = require('../../models/Company');
-const StatsModel = require('../../models/Stats');
+import Command from '../../structures/Command.js'
+import { ButtonBuilder, ButtonStyle, ActionRowBuilder, ComponentType } from 'discord.js'
+import { getBusiness, addMoney } from '../../lib/user.js'
+import { randomNumber, timeout } from '../../lib/helpers.js'
+import { createMessageComponentCollector } from '../../lib/embed.js'
+import Member from '../../models/Member.js'
+import Business from '../../models/Business.js'
+import Stats from '../../models/Stats.js'
+import workData from "../../assets/jobs.json" assert { type: "json" }
+import words from "../../assets/wordlist.json" assert { type: "json" }
 
-class Work extends Command {
+const workList = workData.jobs;
+const wordList = words.words;
+
+export default class extends Command {
     info = {
         name: "work",
         description: "Work hard and get paid a unfair salary.",
         options: [],
         category: "economy",
         extraFields: [],
-        cooldown: 1800,
+        cooldown: 3600,
         enabled: true,
         memberRequired: true,
         deferReply: false
@@ -33,15 +39,15 @@ class Work extends Command {
         data.hasBusiness = false;
         data.gameInProgress = true;
         if (data.user.job.startsWith("business")) {
-            data = await bot.tools.hasCompany(interaction.member.id, data);
-            data.salary = data.employee.wage;
+            data.company = await getBusiness(data.user);
+            data.salary = data.company.employee.wage;
             data.hasBusiness = true;
 
-            if (data.employee.wage > data.company.balance) return await interaction.editReply({ content: `Your company hasn't enough money to pay you. Please produce items in the factories.` });
+            if (data.salary > data.company.company.balance) return await interaction.editReply({ content: `Your business hasn't enough money to pay you. Please produce items in the factories.` });
         } else {
             const job = this.getJob(data.user.job);
             if (job == null) {
-                await MemberModel.updateOne({ id: interaction.member.id }, { $set: { job: "" } });
+                await Member.updateOne({ id: interaction.member.id }, { $set: { job: "" } });
                 await bot.cooldown.removeCooldown(interaction.member.id, this.info.name);
                 return await interaction.editReply({ content: `You don't have a valid job... Please apply for a LEGAL job next time. You can't work if you dont have a LEGAL job.` });
             }
@@ -49,17 +55,17 @@ class Work extends Command {
             data.salary = job.salary;
         }
 
-        const minigames = ["mgRememberWord"]//, "mgMath", "mgOrder"];
-        await eval(`this.${minigames[bot.tools.randomNumber(0, minigames.length - 1)]}(interaction, data)`);
+        const minigames = ["mgRememberWord", "mgMath", "mgOrder"];
+        await eval(`this.${minigames[randomNumber(0, minigames.length - 1)]}(interaction, data)`);
     }
 
     async mgMath(interaction, data) {
         const operators = ["+", "-", "*"];
-        const operatorIndex = bot.tools.randomNumber(0, operators.length - 1);
+        const operatorIndex = randomNumber(0, operators.length - 1);
 
         let maxNumber = operators[operatorIndex] === "*" ? 10 : 50;
-        let number1 = bot.tools.randomNumber(1, maxNumber);
-        let number2 = bot.tools.randomNumber(0, maxNumber);
+        let number1 = randomNumber(1, maxNumber);
+        let number2 = randomNumber(0, maxNumber);
 
         const mathStr = `${number1} ${operators[operatorIndex]} ${number2}`;
         data.answer = eval(mathStr.replace(/[^-()\d/*+.]/g, ''));
@@ -70,41 +76,41 @@ class Work extends Command {
             let fakeNumber2 = 0;
             let fakeAnswer = `0`;
             do {
-                fakeNumber1 = bot.tools.randomNumber(1, maxNumber);
-                fakeNumber2 = bot.tools.randomNumber(0, maxNumber);
+                fakeNumber1 = randomNumber(1, maxNumber);
+                fakeNumber2 = randomNumber(0, maxNumber);
                 fakeAnswer = eval(`${fakeNumber1}${operators[operatorIndex]}${fakeNumber2}`.replace(/[^-()\d/*+.]/g, ''));
             } while (fakeAnswer == data.answer);
             buttons.push({ name: fakeAnswer });
         }
 
         const insert = (arr, index, newItem) => [...arr.slice(0, index), newItem, ...arr.slice(index)];
-        data.correctAnswerIndex = bot.tools.randomNumber(0, 3);
+        data.correctAnswerIndex = randomNumber(0, 3);
         buttons = insert(buttons, data.correctAnswerIndex, { name: data.answer });
 
         const interactionMessage = await interaction.editReply({ content: `What is the result of \`${mathStr}\`?`, components: [this.setButtonsRow(buttons)], fetchReply: true });
-        const collector = bot.tools.createMessageComponentCollector(interactionMessage, interaction, { time: 10000 });
+        const collector = createMessageComponentCollector(interactionMessage, interaction, { time: 10000, componentType: ComponentType.Button });
         await this.collectorOnCollect(collector, interaction, data, buttons, `GG! You are very good at math. You earned :coin: ${data.salary} this hour.`, `You were too slow to pick the correct answer. You didn't earn anything.`);
     }
 
     async mgRememberWord(interaction, data) {
-        data.answer = wordList[bot.tools.randomNumber(0, wordList.length - 1)];
+        data.answer = wordList[randomNumber(0, wordList.length - 1)];
 
         let buttons = [];
         for (let i = 0; i < 5; i++) {
             let wordIndex;
             do {
-                wordIndex = bot.tools.randomNumber(0, wordList.length - 1);
+                wordIndex = randomNumber(0, wordList.length - 1);
             } while (buttons.includes(wordList[wordIndex]) || wordList[wordIndex] === data.answer);
 
             buttons.push({ name: wordList[wordIndex] });
         }
-        data.correctAnswerIndex = bot.tools.randomNumber(0, buttons.length - 1);
+        data.correctAnswerIndex = randomNumber(0, buttons.length - 1);
         buttons[data.correctAnswerIndex] = { name: data.answer };
 
         await interaction.editReply({ content: `Remember this word: \`${data.answer}\`` });
-        await bot.tools.timeout(3000);
+        await timeout(3000);
         const interactionMessage = await interaction.editReply({ content: `What was the word?`, components: [this.setButtonsRow(buttons)], fetchReply: true });
-        const collector = bot.tools.createMessageComponentCollector(interactionMessage, interaction, { time: 10000 });
+        const collector = createMessageComponentCollector(interactionMessage, interaction, { time: 10000, componentType: ComponentType.Button });
         await this.collectorOnCollect(collector, interaction, data, buttons, `GG! You are very good at remembering words. You earned :coin: ${data.salary} this hour.`, `You were too slow to pick the correct answer. You didn't earn anything.`);
     }
 
@@ -117,7 +123,7 @@ class Work extends Command {
         for (let i = 0; i < 5; i++) {
             let buttonName;
             do {
-                buttonName = bot.tools.randomNumber(1, 100);
+                buttonName = randomNumber(1, 100);
             } while (buttonNames.includes(buttonName));
 
             buttonNames.push(buttonName);
@@ -126,7 +132,7 @@ class Work extends Command {
 
         buttonNames.sort((a, b) => a - b);
         const interactionMessage = await interaction.editReply({ content: `Press the buttons in the correct order from low to high!`, components: [this.setButtonsRow(buttons)], fetchReply: true });
-        const collector = bot.tools.createMessageComponentCollector(interactionMessage, interaction, { max: 6, time: 10000 });
+        const collector = createMessageComponentCollector(interactionMessage, interaction, { max: 6, time: 10000, componentType: ComponentType.Button });
 
         collector.on('collect', async (interactionCollector) => {
             await interactionCollector.deferUpdate();
@@ -147,10 +153,18 @@ class Work extends Command {
 
                     if (data.currentButton === buttons.length) {
                         data.gameInProgress = false;
-                        await bot.tools.addMoney(interaction.member.id, data.salary);
+                        await addMoney(interaction.member.id, data.salary);
                         await interaction.editReply({ components: [this.setButtonsRow(buttons, true)] });
-                        if (data.hasBusiness) await CompanyModel.updateOne({ id: data.company.id }, { $inc: { balance: -data.salary } });
-                        return await interaction.followUp({ content: `GG! You ordered these buttons correctly and earned :coin: ${data.salary} this hour.` })
+                        if (data.hasBusiness) await Business.updateOne({ ownerId: data.company.company.ownerId }, { $inc: { balance: -data.salary } });
+                        await interaction.followUp({ content: `GG! You ordered these buttons correctly and earned :coin: ${data.salary} this hour.` });
+
+                        await Stats.updateOne(
+                            { id: interaction.member.id },
+                            { $inc: { workComplete: 1 } },
+                            { upsert: true }
+                        );
+
+                        return;
                     }
 
                     await interaction.editReply({ components: [this.setButtonsRow(buttons)] });
@@ -181,11 +195,11 @@ class Work extends Command {
 
                     await interaction.followUp({ content: `That is not the correct answer. You did not earn anything this hour.` });
                 } else {
-                    await bot.tools.addMoney(interaction.member.id, data.salary);
-                    if (data.hasBusiness) await CompanyModel.updateOne({ id: data.company.id }, { $inc: { balance: -data.salary } });
+                    await addMoney(interaction.member.id, data.salary);
+                    if (data.hasBusiness) await Business.updateOne({ ownerId: data.company.company.ownerId }, { $inc: { balance: -data.salary } });
                     await interaction.followUp({ content: winMsg });
 
-                    await StatsModel.updateOne(
+                    await Stats.updateOne(
                         { id: interaction.member.id },
                         { $inc: { workComplete: 1 } },
                         { upsert: true }
@@ -245,5 +259,3 @@ class Work extends Command {
         return row;
     }
 }
-
-module.exports = Work;
