20c08baf9f6d6305da1750e7ae368be4b1e0aa52
Siebe Baree <siebe.baree@outlook.com>
2022-12-07 21:45:50 +0100

    Added AutoPoster

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { ShardingManager } from 'discord.js';
+import { AutoPoster } from "topgg-autoposter";
 import loggerStruct from './structures/Logger.js';
 
 const manager = new ShardingManager('./src/bot.js', { token: process.env.DISCORD_TOKEN });
 const logger = new loggerStruct(manager);
 
 logger.log(`Running bot in ${process.env.NODE_ENV} mode.`);
+
+if (process.env.NODE_ENV === "production") AutoPoster(process.env.API_TOPGG, manager);
 manager.on('shardCreate', shard => logger.load(`Loading shard ${shard.id}.`));
 manager.spawn();
