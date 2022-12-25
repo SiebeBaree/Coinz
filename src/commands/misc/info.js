ba6c066484c5f19d1de01da9ee6d1e482e6db869
Siebe Baree <siebe.baree@outlook.com>
2022-12-25 10:41:03 +0100

    Updated shard statistic

--- src/commands/misc/info.js
+++ src/commands/misc/info.js
@@ -33,7 +33,7 @@ export default class extends Command {
             .setDescription('**If you like this bot, maybe consider buying [Coinz Premium](https://coinzbot.xyz/store).**')
             .addFields(
                 { name: 'Info', value: `:man_technologist: **Owner:** \`Siebe#0001\`\n:globe_with_meridians: **Website: [coinzbot.xyz](${bot.config.website})**\n:beginner: **Official Server: [discord.gg/asnZQwc6kW](https://discord.gg/asnZQwc6kW)**\n:books: **Library:** \`discord.js ${version}\`\n:star: **Version:** \`${packageFile.version}\``, inline: true },
-                { name: 'Statistics', value: `:video_game: **Commands:** \`${bot.commands.size}\`\n:spider_web: **Shard:** \`${interaction.guild.shardId + 1}/${bot.shard.count}\`\n:white_check_mark: **Uptime:** \`${msToTime(bot.uptime)}\`\n:bust_in_silhouette: **Users:** \`${stats.members}\`\n:printer: **Servers:** \`${stats.guilds}\``, inline: true },
+                { name: 'Statistics', value: `:video_game: **Commands:** \`${bot.commands.size}\`\n:spider_web: **Shard:** \`${interaction.guild.shardId + 1}/${bot.cluster.info.TOTAL_SHARDS}\`\n:white_check_mark: **Uptime:** \`${msToTime(bot.uptime)}\`\n:bust_in_silhouette: **Users:** \`${stats.members}\`\n:printer: **Servers:** \`${stats.guilds}\``, inline: true },
                 { name: 'Disclaimer', value: `Icons from the shop are from [icons8](https://icons8.com).`, inline: false }
             )
         return await interaction.editReply({ embeds: [newEmbed] });
