77381803505f07b788f68f407fe56dcc67a79d26
Siebe Baree <siebe.baree@outlook.com>
2022-12-30 16:52:58 +0100

    Fixed type error

--- src/interfaces/ICommand.ts
+++ src/interfaces/ICommand.ts
@@ -1,12 +1,12 @@
 /* eslint-disable semi */
-import { APIApplicationCommandOption, APIEmbedField, CommandInteraction } from "discord.js";
+import { CommandInteractionOption, APIEmbedField, CommandInteraction } from "discord.js";
 import { IMember } from "../models/Member";
 
 export default interface ICommand {
     readonly info: {
         name: string;
         description: string;
-        options: APIApplicationCommandOption[];
+        options: CommandInteractionOption[];
         category: string;
         extraFields?: APIEmbedField[],
         examples?: string[];
