073006133ae9c3298c83b94404750caae5170bc9
Siebe Baree <siebe.baree@outlook.com>
2023-01-02 18:00:11 +0100

    Fixed UI bug

--- src/commands/misc/Info.ts
+++ src/commands/misc/Info.ts
@@ -30,7 +30,7 @@ export default class extends Command implements ICommand {
             .setDescription("**If you like this bot, maybe consider buying [Coinz Premium](https://coinzbot.xyz/store).**")
             .addFields(
                 { name: "Info", value: `:man_technologist: **Owner:** \`Siebe#0001\`\n:globe_with_meridians: **Website: [coinzbot.xyz](${this.client.config.website})**\n:beginner: **Official Server: [discord.gg/asnZQwc6kW](https://discord.gg/asnZQwc6kW)**\n:books: **Library:** \`discord.js ${version}\``, inline: true },
-                { name: "Statistics", value: `:video_game: **Commands:** \`${this.client.commands.size}\`\n:spider_web: **Shard:** \`${interaction.guild?.shardId ?? 0 + 1}/${this.client.cluster?.info.TOTAL_SHARDS}\`\n:white_check_mark: **Uptime:** \`${Helpers.msToTime(this.client.uptime ?? 0)}\`\n:bust_in_silhouette: **Users:** \`${stats.members}\`\n:printer: **Servers:** \`${stats.guilds}\``, inline: true },
+                { name: "Statistics", value: `:video_game: **Commands:** \`${this.client.commands.size}\`\n:spider_web: **Shard:** \`${(interaction.guild?.shardId ?? 0) + 1}/${this.client.cluster?.info.TOTAL_SHARDS}\`\n:white_check_mark: **Uptime:** \`${Helpers.msToTime(this.client.uptime ?? 0)}\`\n:bust_in_silhouette: **Users:** \`${stats.members}\`\n:printer: **Servers:** \`${stats.guilds}\``, inline: true },
                 { name: "Disclaimer", value: "Icons from the shop are from [icons8](https://icons8.com).", inline: false },
             );
         await interaction.editReply({ embeds: [embed] });
