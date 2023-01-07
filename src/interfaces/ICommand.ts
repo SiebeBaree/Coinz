e08f63d2aa1e20059950e5b9bbf807b4e6cabd64
Siebe Baree <siebe.baree@outlook.com>
2023-01-07 20:33:46 +0100

    Added info interface

--- src/interfaces/ICommand.ts
+++ src/interfaces/ICommand.ts
@@ -2,21 +2,22 @@
 import { CommandInteractionOption, APIEmbedField, ChatInputCommandInteraction } from "discord.js";
 import { IMember } from "../models/Member";
 
-export default interface ICommand {
-    readonly info: {
-        name: string;
-        description: string;
-        helpDescription?: string;
-        options: CommandInteractionOption[];
-        category: string;
-        extraFields?: APIEmbedField[],
-        examples?: string[];
-        cooldown?: number;
-        enabled?: boolean;
-        deferReply?: boolean;
-        isPremium?: boolean;
-        image?: string;
-    };
+export interface Info {
+    name: string;
+    description: string;
+    helpDescription?: string;
+    options: CommandInteractionOption[];
+    category: string;
+    extraFields?: APIEmbedField[],
+    examples?: string[];
+    cooldown?: number;
+    enabled?: boolean;
+    deferReply?: boolean;
+    isPremium?: boolean;
+    image?: string;
+}
 
+export default interface ICommand {
+    readonly info: Info;
     execute: (interaction: ChatInputCommandInteraction, member: IMember) => Promise<void>;
 }
