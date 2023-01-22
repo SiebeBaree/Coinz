ba15c6845736c7ab235a9f6718fc2ce6326b683d
Siebe Baree <siebe.baree@outlook.com>
2023-01-22 16:23:59 +0100

    Added bio and age support

--- src/commands/general/Profile.ts
+++ src/commands/general/Profile.ts
@@ -80,7 +80,7 @@ export default class extends Command implements ICommand {
         });
     }
 
-    calculateInventoryValue(inventory: InventoryItem[]): ICalulatedObject {
+    private calculateInventoryValue(inventory: InventoryItem[]): ICalulatedObject {
         const returnObj = { value: 0, items: 0 };
 
         for (const invItem of inventory) {
@@ -94,7 +94,7 @@ export default class extends Command implements ICommand {
         return returnObj;
     }
 
-    async calculateInvestmentsValue(investments: IInvestment[]): Promise<ICalulatedObject> {
+    private async calculateInvestmentsValue(investments: IInvestment[]): Promise<ICalulatedObject> {
         const returnObj = { value: 0, items: 0, initialValue: 0 };
 
         for (const investment of investments) {
@@ -109,7 +109,7 @@ export default class extends Command implements ICommand {
         return returnObj;
     }
 
-    createProfileEmbed(user: User, userData: IMember, inventory: ICalulatedObject, stocks: ICalulatedObject, workJob: string, businessJob: string): EmbedBuilder {
+    private createProfileEmbed(user: User, userData: IMember, inventory: ICalulatedObject, stocks: ICalulatedObject, workJob: string, businessJob: string): EmbedBuilder {
         const createProgressBar = (current: number): string => {
             const progress = Math.round(current / 10);
             const bar = [];
@@ -123,23 +123,28 @@ export default class extends Command implements ICommand {
             return bar.join("");
         };
 
+        let bio = "";
+        bio += userData.birthday.getTime() > 0 ? `**Age:** ${this.getAge(userData.birthday)} years old` : "";
+        bio += userData.bio.length > 0 ? `\n**Bio:** ${userData.bio}` : "";
+
         const expireTimestamp = userData.premium.expires > Math.floor(Date.now() / 1000) ? userData.premium.expires : 0;
         const embed = new EmbedBuilder()
             .setTitle(`${user.username}'s profile ${userData.displayedBadge === "" ? "" : `<:${userData.displayedBadge}:${Achievement.getById(userData.displayedBadge)?.emoji}>`}`)
             .setColor(<ColorResolvable>(userData.profileColor || this.client.config.embed.color))
+            .setDescription(bio.length > 0 ? bio : null)
             .setThumbnail(user.displayAvatarURL() || this.client.config.embed.icon)
             .addFields(
                 { name: "Experience", value: `:beginner: **Level:** \`${UserUtil.getLevel(userData.experience)}\`\n:game_die: **Next Level:** \`${userData.experience % 100}%\`\n${createProgressBar(userData.experience % 100)}`, inline: false },
-                { name: "Premium Status", value: userData.premium.active && expireTimestamp > 0 ? `:star: **Premium:** :white_check_mark:\n:hourglass: **Premium Expires:** <t:${expireTimestamp}:D>\n` : ":star: **Premium:** :x:", inline: false },
+                { name: "Premium Status", value: userData.premium.active ? `:star: **Premium:** :white_check_mark:\n:hourglass: **Premium Expires:** <t:${expireTimestamp}:D>\n` : ":star: **Premium:** :x:", inline: false },
                 { name: "Balance", value: `:dollar: **Wallet:** :coin: ${userData.wallet}\n:bank: **Bank:** :coin: ${userData.bank} / ${userData.bankLimit}\n:moneybag: **Net Worth:** :coin: ${userData.wallet + userData.bank}\n:credit_card: **Tickets:** <:ticket:1032669959161122976> ${userData.tickets || 0}\n:gem: **Inventory Worth:** \`${inventory.items} items\` valued at :coin: ${inventory.value}`, inline: false },
-                { name: "Investment Portfolio", value: `:dollar: **Worth:** :coin: ${stocks.value}\n:credit_card: **Amount:** ${stocks.items}\n:moneybag: **Invested:** :coin: ${stocks.initialValue}`, inline: false },
+                { name: "Investment Portfolio", value: `:dollar: **Worth:** :coin: ${Math.round((stocks.value + Number.EPSILON) * 100) / 100}\n:credit_card: **Amount:** ${Math.round((stocks.items + Number.EPSILON) * 100) / 100}x\n:moneybag: **Invested:** :coin: ${stocks.initialValue}`, inline: false },
                 { name: "Misc", value: `:briefcase: **Current Job:** ${workJob}\n:office: **Business:** ${businessJob}\n:sparkles: **Daily Streak:** ${userData.streak - 1 > 0 ? userData.streak - 1 : 0} days\n:seedling: **Farm:** ${userData.plots.length} plots`, inline: false },
                 { name: "Badges (Achievements)", value: `${userData.badges.length <= 0 ? "None" : userData.badges.map((id) => `<:${id}:${Achievement.getById(id)?.emoji}>`).join(" ")}`, inline: false },
             );
         return embed;
     }
 
-    async createCooldownEmbed(user: User, profileColor: string): Promise<EmbedBuilder> {
+    private async createCooldownEmbed(user: User, profileColor: string): Promise<EmbedBuilder> {
         const cooldowns = await Cooldown.find({ id: user.id });
 
         let cooldownStr = "";
@@ -171,7 +176,7 @@ export default class extends Command implements ICommand {
         return embed;
     }
 
-    createSelectMenu(selectedOption: string, isDisabled = false): ActionRowBuilder<StringSelectMenuBuilder>[] {
+    private createSelectMenu(selectedOption: string, isDisabled = false): ActionRowBuilder<StringSelectMenuBuilder>[] {
         const row = new ActionRowBuilder<StringSelectMenuBuilder>()
             .addComponents(
                 new StringSelectMenuBuilder()
@@ -197,4 +202,7 @@ export default class extends Command implements ICommand {
             );
         return [row];
     }
+
+    private getAge = (birthday: Date) =>
+        new Date().getFullYear() - birthday.getFullYear() - (new Date() < new Date(new Date().getFullYear(), birthday.getMonth(), birthday.getDate()) ? 1 : 0);
 }
