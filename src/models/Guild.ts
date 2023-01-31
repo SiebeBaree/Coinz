8ba89e648885a8dff7e32929dc9f849e8fac3636
Siebe Baree <siebe.baree@outlook.com>
2023-01-31 17:25:58 +0100

    Removed airdrop

--- src/models/Guild.ts
+++ src/models/Guild.ts
@@ -1,17 +1,6 @@
 import { Schema, model } from "mongoose";
 import { embed } from "../assets/config.json";
 
-interface IAirdrop {
-    status: boolean;
-    channel: string;
-    nextDrop: number;
-    interval: {
-        min: number;
-        max: number;
-    };
-    tries: number;
-}
-
 export interface IPremium {
     active: boolean;
     userId: string;
@@ -21,21 +10,9 @@ export interface IPremium {
 export interface IGuild {
     id: string;
     banned: boolean;
-    airdrop: IAirdrop;
     embedColor: string;
     premium: IPremium;
 }
-
-const Airdrop = new Schema<IAirdrop>({
-    status: { type: Boolean, default: false },
-    channel: { type: String, default: "" },
-    nextDrop: { type: Number, default: 0 },
-    interval: {
-        min: { type: Number, default: 3600 },
-        max: { type: Number, default: 43200 },
-    },
-    tries: { type: Number, default: 0 },
-});
 
 const Premium = new Schema<IPremium>({
     active: { type: Boolean, default: false },
@@ -46,7 +23,6 @@ const Premium = new Schema<IPremium>({
 const guildSchema = new Schema<IGuild>({
     id: { type: String, required: true, unique: true, index: true },
     banned: { type: Boolean, default: false },
-    airdrop: { type: Airdrop, default: {} },
     embedColor: { type: String, default: embed.color },
     premium: { type: Premium, default: {} },
 }, { timestamps: true });
