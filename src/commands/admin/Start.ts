fdb4172ce5b7f6eb33345ff6fb3f9ddbdedb0370
Siebe Baree <siebe.baree@outlook.com>
2023-02-05 18:19:23 +0100

    Added XP and tickets

--- src/commands/admin/Start.ts
+++ src/commands/admin/Start.ts
@@ -2,7 +2,7 @@ import { ChatInputCommandInteraction } from "discord.js";
 import Bot from "../../structs/Bot";
 import ICommand from "../../interfaces/ICommand";
 import Command from "../../structs/Command";
-import User from "../../utils/User";
+import Member from "../../models/Member";
 
 export default class extends Command implements ICommand {
     readonly info = {
@@ -18,7 +18,16 @@ export default class extends Command implements ICommand {
 
     async execute(interaction: ChatInputCommandInteraction) {
         if (interaction.guildId !== this.client.config.adminServerId || process.env.NODE_ENV === "production") return;
-        await User.addMoney(interaction.user.id, 50_000);
-        await interaction.reply({ content: "You have been given :coin: 50,000.", ephemeral: true });
+
+        this.client.logger.info(`Giving ${interaction.user.tag} a starting balance.`);
+        await Member.updateOne({ id: interaction.user.id }, {
+            $inc: {
+                balance: 50000,
+                tickets: 150,
+                experience: 500,
+            },
+        }, { upsert: true });
+
+        await interaction.reply({ content: "You have been given :coin: 50,000, 5 Levels of XP and <:ticket:1032669959161122976> 150 Tickets.", ephemeral: true });
     }
 }
