32f9cab679540166784caba24c26cc501b9c48f7
Siebe Baree <siebe.baree@outlook.com>
2023-05-01 16:53:47 +0200

    Changed store to donate link

--- src/commands/misc/Info.ts
+++ src/commands/misc/Info.ts
@@ -24,7 +24,7 @@ export default class extends Command implements ICommand {
             .setColor(<ColorResolvable>this.client.config.embed.color)
             .setFooter({ text: this.client.config.embed.footer })
             .setTimestamp()
-            .setDescription("**If you like this bot, maybe consider buying [Coinz Premium](https://coinzbot.xyz/store).**")
+            .setDescription("**If you like this bot, maybe consider [donating](https://coinzbot.xyz/donate).**")
             .addFields(
                 { name: "Info", value: `:man_technologist: **Owner:** \`Siebe#0001\`\n:globe_with_meridians: **Website: [coinzbot.xyz](${this.client.config.website})**\n:beginner: **Official Server: [discord.gg/asnZQwc6kW](https://discord.gg/asnZQwc6kW)**\n:books: **Library:** \`discord.js ${version}\``, inline: true },
                 { name: "Statistics", value: `:video_game: **Commands:** \`${this.client.commands.size}\`\n:spider_web: **Shard:** \`${(interaction.guild?.shardId ?? 0) + 1}/${this.client.cluster?.info.TOTAL_SHARDS}\`\n:white_check_mark: **Uptime:** \`${Helpers.msToTime(this.client.uptime ?? 0)}\``, inline: true },
