c95284895d2874742f538769fa9d99360a35fba4
Siebe Baree <siebe.baree@outlook.com>
2023-11-19 16:33:05 +0100

    Fixed footer positioning

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import React from "react";
 import Navbar from "@/components/Navbar";
 import AuthProvider from "@/components/AuthProvider";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,12 @@ export default function RootLayout({ children }: {
         <body className={inter.className}>
         <AuthProvider>
             <Navbar/>
-            {children}
+            <main className="container mx-auto px-5 pb-12" style={{
+                minHeight: "calc(100vh - 60px - 100px)",
+            }}>
+                {children}
+            </main>
+            <Footer/>
         </AuthProvider>
         </body>
         </html>
