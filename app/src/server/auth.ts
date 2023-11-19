46afa21ed07bb6c18b954ec47cd33c85f4624603
Siebe Baree <siebe.baree@outlook.com>
2023-11-19 16:55:52 +0100

    Initial commit

---
+++ app/src/server/auth.ts
@@ -0,0 +1,43 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import {
+    getServerSession,
+    type DefaultSession,
+    type NextAuthOptions,
+} from "next-auth";
+import { db } from "@/server/db";
+import Discord from "next-auth/providers/discord";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+    interface Session extends DefaultSession {
+        user: {
+            id: string;
+        } & DefaultSession["user"];
+    }
+}
+
+export const authOptions: NextAuthOptions = {
+    callbacks: {
+        session: ({ session, user }) => ({
+            ...session,
+            user: {
+                ...session.user,
+                id: user.id,
+            },
+        }),
+    },
+    adapter: PrismaAdapter(db),
+    providers: [
+        Discord({
+            clientId: process.env.DISCORD_CLIENT_ID!,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+        }),
+    ],
+};
+
+export const getServerAuthSession = () => getServerSession(authOptions);
