c7f82f004fec07f5dd9e0f2fccc9ea11cffaaa9c
Siebe Baree <siebe.baree@outlook.com>
2023-11-20 13:39:07 +0100

    Added temporary command loader

--- bot/src/events/Ready.ts
+++ bot/src/events/Ready.ts
@@ -1,11 +1,45 @@
 import IEvent from "../domain/IEvent";
 import Bot from "../domain/Bot";
+import { CommandInteractionOption, REST, Routes } from "discord.js";
+
+interface CommandOptions {
+    name: string;
+    description: string;
+    options?: CommandInteractionOption[];
+    dm_permission?: boolean;
+    default_member_permissions?: string;
+}
 
 export default class Ready implements IEvent {
     public readonly name = "ready";
     public readonly once = true;
 
     async execute(client: Bot) {
-        client.logger.info(`Cluster ${client.cluster?.id} is ready!`);
+        client.logger.debug(`Cluster ${client.cluster?.id} is ready!`);
+
+        // TODO: Remove development code and create proper command uploader
+        if (process.env.NODE_ENV !== "production") {
+            try {
+                const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
+
+                const publicCommands: CommandOptions[] = [];
+                client.commands.forEach((command) => {
+                    publicCommands.push({
+                        name: command.info.name,
+                        description: command.info.description,
+                        options: command.info.options,
+                        dm_permission: false,
+                    });
+                });
+
+                if (process.env.NODE_ENV !== "production") {
+                    client.logger.debug(`Started refreshing ${publicCommands.length} application (/) commands.`);
+                    await rest.put(Routes.applicationGuildCommands(client.user!.id, client.config.adminServerId), { body: publicCommands });
+                    client.logger.debug(`Successfully reloaded ${publicCommands.length} public application (/) commands.`);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
     }
 }
