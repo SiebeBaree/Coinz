63cabb041149f55c881db5d2f638cb6eb3e56a68
Siebe Baree <siebe.baree@outlook.com>
2023-11-28 11:20:25 +0100

    Updated business schema to v4

--- bot/src/models/Business.ts
+++ bot/src/models/Business.ts
@@ -1,14 +1,6 @@
 import { Schema, model } from 'mongoose';
 import type { InventoryItem } from '../lib/types';
 
-export type IEmployee = {
-    userId: string;
-    role: string;
-    payout: number;
-    hiredOn: number;
-    moneyEarned: number;
-};
-
 export type IFactory = {
     factoryId: number;
     level: number;
@@ -18,22 +10,12 @@ export type IFactory = {
 };
 
 export type IBusiness = {
+    userId: string;
     name: string;
-    balance: number;
-    taxRate: number;
-    risk: number;
-    employees: IEmployee[];
+    employees: number;
     inventory: InventoryItem[];
     factories: IFactory[];
 };
-
-const Employee = new Schema<IEmployee>({
-    userId: { type: String, required: true },
-    role: { type: String, default: 'employee' },
-    payout: { type: Number, default: 15, min: 10, max: 100 },
-    hiredOn: { type: Number, default: Math.floor(Date.now() / 1_000) },
-    moneyEarned: { type: Number, default: 0 },
-});
 
 const Item = new Schema<InventoryItem>({
     itemId: { type: String, required: true },
@@ -50,15 +32,12 @@ const Factory = new Schema<IFactory>({
 
 export const businessSchema = new Schema<IBusiness>(
     {
+        userId: { type: String, required: true, unique: true, index: true },
         name: { type: String, required: true, unique: true, index: true },
-        balance: { type: Number, default: 0 },
-        taxRate: { type: Number, default: 0 },
-        risk: { type: Number, default: 0 },
-        employees: [{ type: Employee, default: [] }],
+        employees: { type: Number, default: 0 },
         inventory: [{ type: Item, default: [] }],
         factories: [{ type: Factory, default: [] }],
     },
-    { timestamps: true },
 );
 
 export default model<IBusiness>('Business', businessSchema);
