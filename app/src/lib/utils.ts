7f407807b486a3bce93952dc64844e7dbc39fd72
Siebe Baree <siebe.baree@outlook.com>
2023-12-02 14:30:40 +0100

    Added method to format large numbers

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -22,3 +22,41 @@ export function copyCode() {
         });
     });
 }
+
+export function formatNumber(num: number): {
+    value: number;
+    suffix: string;
+} {
+    // If the number is less than 20,000, return it as is
+    if (num < 20000) {
+        if (num >= 10000) {
+            return {
+                // Round the number to the nearest 1000
+                value: Math.round(num / 1000) * 1000,
+                suffix: '',
+            };
+        } else {
+            return {
+                // Round the number to the nearest 100
+                value: Math.round(num / 100) * 100,
+                suffix: '',
+            };
+        }
+    }
+
+    // Round the number to the nearest 10,000
+    const rounded = Math.round(num / 10000) * 10000;
+
+    // Format the number in 'K' or 'M'
+    if (rounded < 1000000) {
+        return {
+            value: rounded / 1000,
+            suffix: 'K',
+        };
+    } else {
+        return {
+            value: Math.round(rounded / 1000000),
+            suffix: 'M',
+        };
+    }
+}
