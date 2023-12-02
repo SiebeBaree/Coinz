0257d9776cec3676fdcda0b6c19e3f47c2de7956
Siebe Baree <siebe.baree@outlook.com>
2023-12-02 14:30:21 +0100

    Added toggle for subscription duration

---
+++ app/src/app/premium/PremiumClient.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import premium from '@/lib/data/premium.json';
+import Image from 'next/image';
+import { Check } from 'lucide-react';
+import { useState } from 'react';
+import { cn } from '@/lib/utils';
+
+interface Subscription {
+    name: string;
+    price: {
+        monthly: number;
+        quarterly: number;
+    };
+    logo: string;
+    perks: string[];
+}
+
+export default function PremiumClient() {
+    const [priceDuration, setPriceDuration] = useState<'monthly' | 'quarterly'>('monthly');
+    const subscriptions = Object.keys(premium) as Array<keyof typeof premium>;
+
+    return (
+        <div>
+            <div className="flex justify-center mb-8">
+                <div className="bg-secondary rounded-md flex relative items-center p-1 text-center w-[300px]">
+                    <div
+                        className="rounded-md absolute bg-primary transition-all duration-300 ease-in-out"
+                        style={{
+                            top: '6px',
+                            left: priceDuration === 'monthly' ? '6px' : 'calc(50% + 6px)',
+                            height: 'calc(100% - 12px)',
+                            width: 'calc(50% - 12px)',
+                        }}
+                    ></div>
+                    <button
+                        onClick={() => setPriceDuration('monthly')}
+                        className={cn(
+                            'w-full font-medium rounded-md py-2 transition-all duration-200 ease-in-out',
+                            priceDuration === 'monthly' && 'text-primary-foreground',
+                        )}
+                    >
+                        Monthly
+                    </button>
+                    <button
+                        onClick={() => setPriceDuration('quarterly')}
+                        className={cn(
+                            'w-full font-medium rounded-md py-2 transition-all duration-200 ease-in-out',
+                            priceDuration === 'quarterly' && 'text-primary-foreground',
+                        )}
+                    >
+                        Quarterly
+                    </button>
+                </div>
+            </div>
+
+            <div
+                className="grid gap-4 mb-12 mt-4"
+                style={{
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(350px, 1fr))',
+                }}
+            >
+                {subscriptions.map((key) => (
+                    <SubscriptionCard key={key} subscription={premium[key]} price={priceDuration} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+function SubscriptionCard({ subscription, price }: { subscription: Subscription; price: 'monthly' | 'quarterly' }) {
+    return (
+        <div className="flex flex-col items-center bg-secondary rounded-md px-8 mt-12">
+            <Image
+                src={subscription.logo}
+                alt={`${subscription.name} logo`}
+                width={120}
+                height={120}
+                className="select-none absolute -top-[60px] border-[10px] rounded-full border-background"
+            />
+            <h2 className="text-4xl font-bold mt-20">{subscription.name}</h2>
+            <h3 className="text-4xl text-primary font-bold my-6">
+                ${subscription.price[price] / 100}{' '}
+                <span className="text-muted text-base">/{price === 'quarterly' ? '3 months' : 'month'}</span>
+            </h3>
+
+            <div className="flex flex-col items-start gap-2 mb-8 w-full">
+                {subscription.perks.map((perk: string, index: number) => (
+                    <div key={index} className="flex gap-2 items-center">
+                        <Check className="text-primary h-5" />
+                        <p key={perk} className="">
+                            {perk}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
--- app/src/app/premium/page.tsx
+++ app/src/app/premium/page.tsx
@@ -1,18 +1,5 @@
-import premium from '@/lib/data/premium.json';
-import { Check } from 'lucide-react';
-import Image from 'next/image';
 import PageTitle from '@/components/PageTitle';
-
-interface Subscription {
-    name: string;
-    price: {
-        month: number;
-        quarter: number;
-        year: number;
-    };
-    logo: string;
-    perks: string[];
-}
+import PremiumClient from '@/app/premium/PremiumClient';
 
 export default function PremiumPage() {
     return (
@@ -22,45 +9,7 @@ export default function PremiumPage() {
                 description="Here, you'll find all the information you need to stay up to date with the status of our bot. We've provided detailed information for every cluster and shard of Coinz, so you can quickly and easily see if everything is running smoothly."
             />
 
-            <div
-                className="grid gap-4 mb-12"
-                style={{
-                    gridTemplateColumns: 'repeat(auto-fit, minmax(350px, 1fr))',
-                }}
-            >
-                {Object.keys(premium).map((key: string) => (
-                    <SubscriptionCard key={key} subscription={premium[key]} />
-                ))}
-            </div>
+            <PremiumClient/>
         </main>
-    );
-}
-
-function SubscriptionCard({ subscription }: { subscription: Subscription }) {
-    return (
-        <div className="flex flex-col items-center bg-secondary rounded-md px-8 mt-12">
-            <Image
-                src={subscription.logo}
-                alt={`${subscription.name} logo`}
-                width={120}
-                height={120}
-                className="select-none absolute -top-[60px] border-[10px] rounded-full border-background"
-            />
-            <h2 className="text-4xl font-bold mt-20">{subscription.name}</h2>
-            <h3 className="text-4xl text-primary font-bold my-6">
-                ${subscription.price.month / 100} <span className="text-muted text-base">/Month</span>
-            </h3>
-
-            <div className="flex flex-col items-start gap-2 mb-8 w-full">
-                {subscription.perks.map((perk: string, index: number) => (
-                    <div key={index} className="flex gap-2 items-center">
-                        <Check className="text-primary h-5" />
-                        <p key={perk} className="">
-                            {perk}
-                        </p>
-                    </div>
-                ))}
-            </div>
-        </div>
     );
 }
