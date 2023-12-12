097ee8f4b3d7c66fea05480cbe1cc17339833845
Siebe Baree <siebe.baree@outlook.com>
2023-12-12 16:26:28 +0100

    Used logger instead of console.log

--- bot/src/events/ready.ts
+++ bot/src/events/ready.ts
@@ -1,10 +1,11 @@
 import { Events } from 'discord.js';
 import type { Event } from '../domain/Event';
+import logger from '../utils/logger';
 
 export default {
     name: Events.ClientReady,
     once: true,
     async execute(_, client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+        logger.info(`Ready! Logged in as ${client.user.tag}`);
     },
 } satisfies Event<Events.ClientReady>;
