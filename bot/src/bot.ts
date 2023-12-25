c6abec2f1b330011ade93e579436a8433f846ecc
Siebe Baree <siebe.baree@outlook.com>
2023-12-25 12:16:47 +0100

    Added sharding stats

--- bot/src/bot.ts
+++ bot/src/bot.ts
@@ -1,7 +1,12 @@
 import process from 'node:process';
+import { setInterval } from 'node:timers';
 import { getInfo } from 'discord-hybrid-sharding';
 import { ActivityType, GatewayIntentBits, Partials } from 'discord.js';
 import { connect } from 'mongoose';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Stats from 'sharding-stats';
 import Bot from './domain/Bot';
 import logger from './utils/logger';
 
@@ -28,6 +33,36 @@ import logger from './utils/logger';
         logger.error(error);
         process.exit(1);
     }
+
+    bot.once('ready', () => {
+        if (process.env.NODE_ENV === 'production') {
+            const statsClient = new Stats.Client(bot, {
+                customPoster: true,
+                authorizationkey: process.env.API_SHARDINGSTATS,
+                stats_uri: process.env.WEBSERVER_URL,
+            });
+
+            const postStats = async () => {
+                const shards = [...bot.ws.shards.values()];
+                const guilds = [...bot.guilds.cache.values()];
+                for (const shard of shards) {
+                    const filteredGuilds = guilds ? guilds.filter((x) => x.shardId === shard.id).filter(Boolean) : [];
+
+                    const body = {
+                        id: shard ? shard.id : -1,
+                        cluster: bot.cluster?.id,
+                        ping: shard ? shard.ping : -1,
+                        guildCount: filteredGuilds.length,
+                        memberCount: filteredGuilds.reduce((a, b) => a + b.memberCount, 0),
+                    };
+
+                    await statsClient.sendPostData(body);
+                }
+            };
+
+            setInterval(postStats, 1000 * 15);
+        }
+    });
 
     await bot.login(process.env.DISCORD_TOKEN!);
 })();
