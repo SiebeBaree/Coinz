192955f0f299c2d2febac21d008181676e3072db
Siebe Baree <siebe.baree@outlook.com>
2023-12-27 18:07:35 +0100

    Removed settings and added it to config

--- support/src/domain/Bot.ts
+++ support/src/domain/Bot.ts
@@ -2,8 +2,6 @@ import { join } from 'node:path';
 import type { ClientOptions } from 'discord.js';
 import { Client, Collection } from 'discord.js';
 import config from '../data/config.json';
-import type { ISettings } from '../models/settings';
-import Settings from '../models/settings';
 import { loadCommands, loadEvents } from '../utils/loaders';
 import logger from '../utils/logger';
 import { registerEvents } from '../utils/registerEvents';
@@ -14,7 +12,6 @@ export default class Bot extends Client {
     public commands: Collection<string, Command>;
     public events: Collection<string, Event>;
     public readonly config: typeof config;
-    private _settings: ISettings;
 
     public constructor(options: ClientOptions) {
         super(options);
@@ -23,19 +20,6 @@ export default class Bot extends Client {
         this.events = new Collection();
 
         this.config = config;
-        this._settings = new Settings({
-            ticketCategory: '1188856897688846346',
-            ticketSupportRole: '1188875038305296445',
-            modLogChannel: '939870751744462868',
-        });
-    }
-
-    public get settings(): ISettings {
-        return this._settings;
-    }
-
-    public set settings(value: ISettings) {
-        this._settings = value;
     }
 
     public override async login(token: string): Promise<string> {
