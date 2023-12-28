0340f043d8c282c9a7c3f0a98d32de6afcb882f0
Siebe Baree <siebe.baree@outlook.com>
2023-12-28 16:18:07 +0100

    Added subscribe button

--- app/src/app/premium/page.tsx
+++ app/src/app/premium/page.tsx
@@ -6,7 +6,7 @@ export default function PremiumPage() {
         <main className="container mx-auto px-5">
             <PageTitle
                 title="Premium"
-                description="Here, you'll find all the information you need to stay up to date with the status of our bot. We've provided detailed information for every cluster and shard of Coinz, so you can quickly and easily see if everything is running smoothly."
+                description="Discover the exclusive benefits of Coinz Plus and Coinz Pro, our premium subscriptions for the Coinz Discord bot. Gain access to more features and priority support!"
             />
 
             <PremiumClient />
