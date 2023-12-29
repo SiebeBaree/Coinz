de2125b420c6a76e927f5f0ce652ee55bab13d46
Siebe Baree <siebe.baree@outlook.com>
2023-12-29 14:28:43 +0100

    Updated session and jwt callbacks

--- app/src/server/auth.ts
+++ app/src/server/auth.ts
@@ -1,31 +1,50 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth';
 import { db } from '@/server/db';
 import Discord from 'next-auth/providers/discord';
+import { User } from '@prisma/client';
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module 'next-auth' {
     interface Session extends DefaultSession {
         user: {
             id: string;
+            discordId: string;
         } & DefaultSession['user'];
     }
 }
 
 export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET!,
     callbacks: {
-        session: ({ session, user }) => ({
-            ...session,
-            user: {
-                ...session.user,
-                id: user.id,
-            },
-        }),
+        session: async ({ session, token }) => {
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id,
+                    discordId: token.discordId,
+                },
+            };
+        },
+        jwt: async ({ token, user }) => {
+            if (user) {
+                const u = user as unknown as User;
+                const account = await db.account.findFirst({
+                    where: { userId: u.id },
+                });
+
+                return {
+                    ...token,
+                    id: u.id,
+                    discordId: account?.providerAccountId,
+                };
+            }
+
+            return token;
+        },
     },
     adapter: PrismaAdapter(db),
     providers: [
