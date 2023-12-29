3e2c68a6c4fcb6a0105dbc988c14c6297c126846
Siebe Baree <siebe.baree@outlook.com>
2023-12-29 11:42:53 +0100

    Moved navbar and footer to main group folder

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,10 +2,7 @@ import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import React from 'react';
-import Navbar from '@/components/nav/Navbar';
 import GlobalProviders from '@/components/GlobalProviders';
-import Footer from '@/components/Footer';
-import { getServerAuthSession } from '@/server/auth';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,23 +11,10 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-    const session = await getServerAuthSession();
-
     return (
         <html lang="en">
             <body className={inter.className}>
-                <GlobalProviders>
-                    <Navbar session={session} />
-                    <div
-                        className="pb-12"
-                        style={{
-                            minHeight: 'calc(100vh - 60px - 310px)',
-                        }}
-                    >
-                        {children}
-                    </div>
-                    <Footer />
-                </GlobalProviders>
+                <GlobalProviders>{children}</GlobalProviders>
             </body>
         </html>
     );
