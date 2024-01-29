ca986f1dbef4899211a6a98f495c912fb4c81a13
Siebe Baree <siebe.baree@outlook.com>
2024-01-29 13:53:56 +0100

    Added better tracking

--- bot/src/utils/money.ts
+++ bot/src/utils/money.ts
@@ -59,6 +59,7 @@ export async function removeBetMoney(
             },
             {
                 $inc: {
+                    totalSpend: bet,
                     'games.moneySpent': bet,
                 },
             },
