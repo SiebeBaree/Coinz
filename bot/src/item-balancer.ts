23c5b1d63496e644eece40bb5cd46c29e98786cf
Siebe Baree <siebe.baree@outlook.com>
2024-03-09 08:23:36 +0100

    Updated item balancer (for testing)

--- bot/src/item-balancer.ts
+++ bot/src/item-balancer.ts
@@ -1,106 +1,84 @@
 type ItemData = {
     name: string;
     amount: number;
-    buyPrice?: number;
-    itemPrice: number;
     sellPrice: number;
     duration: number;
+    priceToProduce: number;
 };
 
 const data: ItemData[] = [
     {
-        name: 'shirt',
-        amount: 3,
-        itemPrice: 45,
-        sellPrice: 21,
-        duration: 2,
-    },
-    {
-        name: 'sneakers',
+        name: 'CPU',
         amount: 2,
-        itemPrice: 75,
-        sellPrice: 53,
-        duration: 4,
+        sellPrice: 150,
+        duration: 32,
+        priceToProduce: 80,
     },
     {
-        name: 'propeller',
-        amount: 4,
-        itemPrice: 98,
-        sellPrice: 35,
-        duration: 6,
-    },
-    {
-        name: 'power cell',
-        amount: 3,
-        itemPrice: 290,
-        sellPrice: 133,
-        duration: 18,
-    },
-    {
-        name: 'cpu',
+        name: 'RAM',
         amount: 2,
-        itemPrice: 160,
-        sellPrice: 118,
-        duration: 12,
+        sellPrice: 45,
+        duration: 8,
+        priceToProduce: 25,
     },
     {
-        name: 'laptop',
+        name: 'Screen',
+        amount: 2,
+        sellPrice: 90,
+        duration: 22,
+        priceToProduce: 20,
+    },
+    {
+        name: 'Engine',
         amount: 1,
-        itemPrice: 5 * 8 + 50 + 160 + 290,
-        sellPrice: 750,
-        duration: 36,
+        sellPrice: 1000,
+        duration: 72,
+        priceToProduce: 25 * 5 + 35 * 12,
     },
-    // {
-    //     name: 'drone',
-    //     amount: 1,
-    //     itemPrice: 870,
-    //     sellPrice: 960,
-    //     duration: 24,
-    // },
-    // {
-    //     name: 'smartphone',
-    //     amount: 1,
-    //     itemPrice: 480,
-    //     sellPrice: 600,
-    //     duration: 24,
-    // },
-    // {
-    //     name: 'solar panel',
-    //     amount: 1,
-    //     itemPrice: 245,
-    //     sellPrice: 310,
-    //     duration: 48,
-    // },
-    // {
-    //     name: 'engine',
-    //     amount: 1,
-    //     itemPrice: 820,
-    //     sellPrice: 1_000,
-    //     duration: 72,
-    // },
-    // {
-    //     name: 'electric car',
-    //     amount: 1,
-    //     itemPrice: 3_870,
-    //     sellPrice: 4_200,
-    //     duration: 120,
-    // },
+    {
+        name: 'Power Cell',
+        amount: 4,
+        sellPrice: 55,
+        duration: 18,
+        priceToProduce: 80,
+    },
+    {
+        name: 'Electric Car',
+        amount: 1,
+        sellPrice: 10_000,
+        duration: 336,
+        priceToProduce: 2000 + 40 * 55 + 20 * 20 + 2 * 150,
+    },
+    {
+        name: 'Server',
+        amount: 1,
+        sellPrice: 2000,
+        duration: 96,
+        priceToProduce: 2 * 150 + 12 * 45,
+    },
+    {
+        name: 'Computer',
+        amount: 1,
+        sellPrice: 1100,
+        duration: 56,
+        priceToProduce: 150 + 4 * 45 + 20 * 8,
+    },
+    {
+        name: 'Smartphone',
+        amount: 1,
+        sellPrice: 600,
+        duration: 24,
+        priceToProduce: 150 + 45 + 90 + 3 * 8 + 55,
+    },
 ];
 
-console.log("Balancing items' buy prices...");
 for (const item of data) {
     const itemsPerWeek = (7 * 24) / item.duration;
-    const profitPerWeek = itemsPerWeek * item.sellPrice * item.amount;
-    const netProfitPerWeek = profitPerWeek - itemsPerWeek * item.itemPrice;
-    const profitWithItemsBoughtPerWeek = profitPerWeek * (item.buyPrice ?? 0);
+    const profitPerWeek = itemsPerWeek * item.sellPrice * item.amount - itemsPerWeek * item.priceToProduce;
 
     console.log(
-        `${item.name}:\n- 1 factory: ${Math.floor(itemsPerWeek)}x ${Math.floor(netProfitPerWeek)} (${Math.floor(
-            profitWithItemsBoughtPerWeek,
-        )})\n- 9 factories: ${Math.floor(itemsPerWeek * 9 * item.amount)}x ${Math.floor(
-            netProfitPerWeek * 9,
-        )} (${Math.floor(profitWithItemsBoughtPerWeek * 9)})\n- 12 factories (PRO): ${Math.floor(
-            itemsPerWeek * 12 * item.amount,
-        )}x ${Math.floor(netProfitPerWeek * 12)} (${Math.floor(profitWithItemsBoughtPerWeek * 12)})\n`,
+        `${item.name}:\n- 1 factory: ${Math.floor(itemsPerWeek)}x ${Math.floor(profitPerWeek)}\n- 8 factories: ${Math.floor(itemsPerWeek * 8 * item.amount)}x ${Math.floor(
+            profitPerWeek * 8,
+        )}\n- 10 factories: ${Math.floor(itemsPerWeek * 10 * item.amount)}x ${Math.floor(profitPerWeek * 10)}\n`,
     );
 }
