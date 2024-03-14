655afefe10244261b117545ae2047950a61e236e
Siebe Baree <siebe.baree@outlook.com>
2024-03-14 17:13:48 +0100

    Removed debug code

--- bot/src/sharder.ts
+++ bot/src/sharder.ts
@@ -3,11 +3,13 @@ import process from 'node:process';
 import { ClusterManager } from 'discord-hybrid-sharding';
 import logger from './utils/logger';
 
+const SHARDS_PER_CLUSTER = Number.parseInt(process.env.SHARDS_PER_CLUSTER!, 10);
+const TOTAL_CLUSTERS = Number.parseInt(process.env.TOTAL_CLUSTERS!, 10);
+
 const manager = new ClusterManager(join(__dirname, 'bot.ts'), {
-    totalShards:
-        Number.parseInt(process.env.SHARDS_PER_CLUSTER!, 10) * Number.parseInt(process.env.TOTAL_CLUSTERS!, 10),
-    shardsPerClusters: Number.parseInt(process.env.SHARDS_PER_CLUSTER!, 10),
-    totalClusters: Number.parseInt(process.env.TOTAL_CLUSTERS!, 10),
+    totalShards: SHARDS_PER_CLUSTER * TOTAL_CLUSTERS,
+    shardsPerClusters: SHARDS_PER_CLUSTER,
+    totalClusters: TOTAL_CLUSTERS,
     mode: 'process',
     token: process.env.DISCORD_TOKEN,
 });
