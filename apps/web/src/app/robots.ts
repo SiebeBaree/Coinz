edadf2131024502a987250e888e4ddc4e01fd5c9
Siebe Baree <siebe.baree@outlook.com>
2024-04-11 20:33:20 +0200

    Add SEO

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,13 @@
+import { env } from '@/env';
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '*',
+            disallow: ['/dashboard/*', '/api/*', '/login'],
+        },
+        sitemap: `${env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+    };
+}
---
+++ apps/web/src/app/sitemap.ts
@@ -0,0 +1,47 @@
+import { MetadataRoute } from 'next';
+import guides from '@/lib/data/guides.json';
+import { env } from '@/env';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const pages = [
+        'commands',
+        'items',
+        'premium',
+        'terms-of-use',
+        'privacy-policy',
+        'investments',
+        'changelog',
+        'guide',
+    ];
+
+    const routes: MetadataRoute.Sitemap = [];
+
+    routes.push({
+        url: env.NEXT_PUBLIC_BASE_URL,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+    });
+
+    for (const page of pages) {
+        routes.push({
+            url: `${env.NEXT_PUBLIC_BASE_URL}/${page}`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        });
+    }
+
+    for (const guide of guides) {
+        if (guide.href) {
+            routes.push({
+                url: `${env.NEXT_PUBLIC_BASE_URL}/guide/${guide.href}`,
+                lastModified: new Date(),
+                changeFrequency: 'monthly',
+                priority: 0.5,
+            });
+        }
+    }
+
+    return routes;
+}
