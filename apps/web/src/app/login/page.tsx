69faffdb52b999a60d9fd38725138582b567e03f
Siebe Baree <siebe.baree@outlook.com>
2024-04-12 11:37:25 +0200

    Fixed build issue

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,18 +1,27 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 export default function LoginPage() {
+    return (
+        <Suspense>
+            <LoginComponent />
+        </Suspense>
+    );
+}
+
+function LoginComponent() {
     const searchParams = useSearchParams();
+    const url = searchParams.get('url');
 
     useEffect(() => {
-        const url = searchParams.get('url');
-
         signIn('discord', {
             callbackUrl: url ?? '/dashboard',
             redirect: true,
         });
-    }, [searchParams]);
+    }, [url]);
+
+    return null; // Render nothing or some placeholder here if needed
 }
