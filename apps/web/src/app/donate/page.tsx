80a5d7b6b2b1c823e1509ce4e9a9190f78a9d91c
Siebe Baree <siebe.baree@outlook.com>
2024-04-16 10:26:00 +0200

    Made improvements to donate page

--- apps/web/src/app/donate/page.tsx
+++ apps/web/src/app/donate/page.tsx
@@ -1,17 +1,5 @@
-import { getCheckoutURL } from '@/actions/lemonsqueezy';
-import { auth } from '@/server/auth';
 import { redirect } from 'next/navigation';
 
-export default async function DonatePage() {
-    const session = await auth();
-    if (!session) {
-        return redirect('https://store.coinzbot.xyz/buy/05235abb-6fcf-4e95-9c07-a78b5a42e2be?discount=0');
-    }
-
-    const checkout = await getCheckoutURL(342282);
-    if (checkout.success && checkout.checkoutUrl) {
-        return redirect(checkout.checkoutUrl);
-    } else {
-        return redirect('https://store.coinzbot.xyz/buy/05235abb-6fcf-4e95-9c07-a78b5a42e2be?discount=0');
-    }
+export default function DonatePage() {
+    return redirect('https://store.coinzbot.xyz/buy/05235abb-6fcf-4e95-9c07-a78b5a42e2be?discount=0');
 }
